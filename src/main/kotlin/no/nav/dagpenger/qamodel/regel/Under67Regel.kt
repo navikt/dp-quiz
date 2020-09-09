0592e364a92dd53f2f45214a871d0bab146670ab
Celine ten Dam <celine.ten.dam@nav.no>
2020-09-09 10:31:38 +0200

    la til test om negativ konklusjon

--- src/main/kotlin/no/nav/dagpenger/qamodel/regel/Under67Regel.kt
+++ src/main/kotlin/no/nav/dagpenger/qamodel/regel/Under67Regel.kt
@@ -3,7 +3,7 @@ package no.nav.dagpenger.qamodel.regel
 import no.nav.dagpenger.qamodel.fakta.Faktum
 import java.time.LocalDate
 
-object VirkningstidspunktRegel: Regel {
+object Under67Regel: Regel {
     override fun konkluder(fakta: List<Faktum<*>>): Boolean {
         return konkluder(fakta.first().svar() as LocalDate, fakta.last().svar() as LocalDate)
     }
--- src/test/kotlin/no/nav/dagpenger/qamodel/unit/SubsumsjonsTest.kt
+++ src/test/kotlin/no/nav/dagpenger/qamodel/unit/SubsumsjonsTest.kt
@@ -2,23 +2,30 @@ package no.nav.dagpenger.qamodel.unit
 
 import no.nav.dagpenger.qamodel.fakta.Faktum
 import no.nav.dagpenger.qamodel.helpers.januar
-import no.nav.dagpenger.qamodel.regel.VirkningstidspunktRegel
+import no.nav.dagpenger.qamodel.regel.Under67Regel
 import no.nav.dagpenger.qamodel.subsumsjon.Subsumsjon
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
 
 internal class SubsumsjonsTest {
 
     val bursdag67 = Faktum<LocalDate>("Datoen du fyller 67")
-
     val søknadsdato = Faktum<LocalDate>("Datoen du søker om dagpenger")
 
-
     @Test
-    fun ` `(){
+    fun `subsumsjonen kan konkludere`(){
+        assertThrows<IllegalStateException>{Subsumsjon(Under67Regel, bursdag67, søknadsdato).konkluder()}
         bursdag67.besvar(31.januar)
         søknadsdato.besvar(1.januar)
-        assertEquals(true, Subsumsjon(VirkningstidspunktRegel, bursdag67, søknadsdato).konkluder())
+        assertTrue(Subsumsjon(Under67Regel, bursdag67, søknadsdato).konkluder())
+    }
+
+    @Test
+    fun `subsumsjonen kan konkludere negativt`(){
+        bursdag67.besvar(1.januar)
+        søknadsdato.besvar(31.januar)
+        assertFalse(Subsumsjon(Under67Regel, bursdag67, søknadsdato).konkluder())
     }
 }
