12281c42fd18f002c2962e0343a4e4812343388f
André Roaldseth <andre.roaldseth@nav.no>
2020-09-14 09:57:39 +0200

    Lag en SammensattSubsumsjon med delt funksjonalitet
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- model/src/main/kotlin/no/nav/dagpenger/model/subsumsjon/MinstEnAvSubsumsjon.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/subsumsjon/MinstEnAvSubsumsjon.kt
@@ -1,13 +1,11 @@
 package no.nav.dagpenger.model.subsumsjon
 
-import no.nav.dagpenger.model.fakta.Faktum
-import no.nav.dagpenger.model.visitor.PrettyPrint
 import no.nav.dagpenger.model.visitor.SubsumsjonVisitor
 
 class MinstEnAvSubsumsjon internal constructor(
     navn: String,
-    private val subsumsjoner: List<Subsumsjon>
-) : Subsumsjon(navn) {
+    subsumsjoner: List<Subsumsjon>
+) : SammensattSubsumsjon(navn, subsumsjoner) {
     override fun konkluder() = subsumsjoner.any { it.konkluder() }
 
     override fun accept(visitor: SubsumsjonVisitor) {
@@ -16,45 +14,5 @@ class MinstEnAvSubsumsjon internal constructor(
         acceptGyldig(visitor)
         acceptUgyldig(visitor)
         visitor.postVisit(this)
-    }
-
-    override fun nesteFakta(): Set<Faktum<*>> =
-        subsumsjoner.flatMap { it.nesteFakta() }.toSet().let {
-            if (it.isNotEmpty()) return it
-            (if (konkluder()) gyldigSubsumsjon else ugyldigSubsumsjon).nesteFakta()
-        }
-
-    override fun _sti(subsumsjon: Subsumsjon): List<Subsumsjon> {
-        if (this == subsumsjon) return listOf(this)
-        subsumsjoner.forEach {
-            it._sti(subsumsjon).also { child ->
-                if (child.isNotEmpty()) return listOf(this) + child
-            }
-        }
-        return emptyList()
-    }
-
-    override fun _resultat(): Boolean? {
-        if (subsumsjoner.any { it._resultat() == null }) return null
-        return subsumsjoner.any { it._resultat()!! }
-    }
-
-    override fun enkelSubsumsjoner(vararg fakta: Faktum<*>): List<EnkelSubsumsjon> =
-        subsumsjoner.flatMap { it.enkelSubsumsjoner(*fakta) } +
-            gyldigSubsumsjon.enkelSubsumsjoner(*fakta) +
-            ugyldigSubsumsjon.enkelSubsumsjoner(*fakta)
-
-    override fun fakta() = subsumsjoner.flatMap { it.fakta() }.toSet() + gyldigSubsumsjon.fakta()
-
-    override fun toString() = PrettyPrint(this).result()
-
-    override operator fun get(indeks: Int) = subsumsjoner[indeks]
-
-    override fun iterator(): Iterator<Subsumsjon> {
-        val iterator = subsumsjoner.iterator()
-        return object : Iterator<Subsumsjon> {
-            override fun hasNext() = iterator.hasNext()
-            override fun next() = iterator.next()
-        }
     }
 }
