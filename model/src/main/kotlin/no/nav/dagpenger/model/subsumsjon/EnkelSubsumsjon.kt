eb8ea8a6ba6b46938c9f0c64dc4280099f52169c
Celine ten Dam <celine.ten.dam@nav.no>
2020-09-14 11:04:54 +0200

    refaktorerte nesteFakta
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- model/src/main/kotlin/no/nav/dagpenger/model/subsumsjon/EnkelSubsumsjon.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/subsumsjon/EnkelSubsumsjon.kt
@@ -6,8 +6,8 @@ import no.nav.dagpenger.model.regel.Regel
 import no.nav.dagpenger.model.visitor.SubsumsjonVisitor
 
 class EnkelSubsumsjon internal constructor(
-    private val regel: Regel,
-    vararg fakta: Faktum<*>
+        private val regel: Regel,
+        vararg fakta: Faktum<*>
 ) : Subsumsjon(regel.toString()) {
     private val fakta = fakta.toSet()
 
@@ -23,20 +23,19 @@ class EnkelSubsumsjon internal constructor(
 
     override fun fakta(): Set<Faktum<*>> = fakta
 
-    override fun nesteFakta(): Set<Faktum<*>> {
-        return mutableSetOf<Faktum<*>>().also {
-            fakta.forEach { faktum -> faktum.leggTilHvis(Faktum.FaktumTilstand.Ukjent, it) }
-            if (it.isNotEmpty()) return it
-            (if (konkluder()) gyldigSubsumsjon else ugyldigSubsumsjon).nesteFakta()
-        }
-    }
+    override fun nesteFakta() = ukjenteFakta().takeIf { it.isNotEmpty() } ?: nesteSubsumsjon().nesteFakta()
+
+    private fun ukjenteFakta() = mutableSetOf<Faktum<*>>().also {
+        fakta.forEach { faktum -> faktum.leggTilHvis(Faktum.FaktumTilstand.Ukjent, it) } }
+
+    private fun nesteSubsumsjon() = if (konkluder()) gyldigSubsumsjon else ugyldigSubsumsjon
 
     override fun _sti(subsumsjon: Subsumsjon) = if (this == subsumsjon) listOf(this) else emptyList()
 
     override fun _resultat() = if (fakta.erBesvart()) konkluder() else null
 
     override fun enkelSubsumsjoner(vararg fakta: Faktum<*>): List<EnkelSubsumsjon> =
-        if (fakta.any { it in this.fakta }) listOf(this) else emptyList()
+            if (fakta.any { it in this.fakta }) listOf(this) else emptyList()
 
     override fun toString() = regel.toString()
 
