353bbe0eccfcea469081c7cadd2885282a1a6ec2
Celine ten Dam <celine.ten.dam@nav.no>
2020-09-17 10:34:48 +0200

    la til uuid på Søknad
    Co-authored-by: Fred George <fredgeorge@acm.org>
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>
    Co-authored-by: Marte Pedersen <marte.pedersen@nav.no>

--- model/src/main/kotlin/no/nav/dagpenger/model/søknad/Søknad.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/søknad/Søknad.kt
@@ -1,8 +1,9 @@
 package no.nav.dagpenger.model.søknad
 
 import no.nav.dagpenger.model.subsumsjon.Subsumsjon
+import java.util.UUID
 
-class Søknad(vararg seksjoner: Seksjon) : Collection<Seksjon> by seksjoner.toList() {
-    private val seksjoner = seksjoner.toList()
+class Søknad private constructor(uuid: UUID, private val seksjoner: List<Seksjon>) : Collection<Seksjon> by seksjoner {
+    constructor(vararg seksjoner: Seksjon) : this(UUID.randomUUID(), seksjoner.toList())
     infix fun nesteSeksjon(subsumsjon: Subsumsjon) = seksjoner.first { subsumsjon.nesteFakta() in it }
 }
