26e05d8fac5f42248028c503025640b86acfb01b
Celine ten Dam <celine.ten.dam@nav.no>
2020-09-17 15:30:02 +0200

    Ny test for å sjekke om avhengige fakta i json
    Co-authored-by: Fred George <fredgeorge@acm.org>
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>
    Co-authored-by: Marte Pedersen <marte.pedersen@nav.no>

--- model/src/test/kotlin/no/nav/dagpenger/model/visitor/JsonBuilderTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/visitor/JsonBuilderTest.kt
@@ -3,13 +3,15 @@ package no.nav.dagpenger.model.visitor
 import no.nav.dagpenger.model.fakta.FaktumNavn
 import no.nav.dagpenger.model.fakta.faktum
 import no.nav.dagpenger.model.regel.har
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 internal class JsonBuilderTest {
 
     @Test
-    fun `Henter faktum id fra json`() {
+    fun `Finner faktum-id i json`() {
         val faktumNavnId = 1
         val faktum = FaktumNavn(faktumNavnId, "faktum").faktum<Boolean>()
 
@@ -17,5 +19,20 @@ internal class JsonBuilderTest {
         println(jsonfakta)
 
         assertEquals(faktumNavnId, jsonfakta["id"].asInt())
+    }
+
+    @Test
+    @Disabled
+    fun `Finner avhengige fakta i json`() {
+        val faktumNavnId = 1
+        val faktum = FaktumNavn(faktumNavnId, "faktum").faktum<Boolean>()
+        val avhengigFaktum = FaktumNavn(2, "faktumto").faktum<Boolean>()
+
+        avhengigFaktum avhengerAv faktum
+
+        val jsonfakta = JsonBuilder(har(faktum)).resultat()
+        println(jsonfakta)
+
+        assertNotNull(jsonfakta["avhengigeFakta"][0])
     }
 }
