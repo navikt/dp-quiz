17b3f779c51bb00b3463bc55800c0bc268d6885a
Celine ten Dam <celine.ten.dam@nav.no>
2020-09-17 14:19:53 +0200

    Lagt til JsonBuilder med visitor pattern. Utvidet SubsumsjonVisitor til å ta med faktum-id
    Co-authored-by: Fred George <fredgeorge@acm.org>
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>
    Co-authored-by: Marte Pedersen <marte.pedersen@nav.no>

--- model/src/main/kotlin/no/nav/dagpenger/model/visitor/JsonBuilder.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/visitor/JsonBuilder.kt
@@ -1,20 +1,16 @@
 package no.nav.dagpenger.model.visitor
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
-import no.nav.dagpenger.model.fakta.Faktum
 import no.nav.dagpenger.model.subsumsjon.Subsumsjon
 
-class JsonBuilder(private val subsumsjon: Subsumsjon): SubsumsjonVisitor {
+class JsonBuilder(private val subsumsjon: Subsumsjon) : SubsumsjonVisitor {
     private val mapper = ObjectMapper()
     private var root: ObjectNode = mapper.createObjectNode()
-
 
     init {
         subsumsjon.accept(this)
     }
 
     fun resultat() = root
-
 }
--- model/src/test/kotlin/no/nav/dagpenger/model/visitor/JsonBuilderTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/visitor/JsonBuilderTest.kt
@@ -3,13 +3,12 @@ package no.nav.dagpenger.model.visitor
 import no.nav.dagpenger.model.fakta.FaktumNavn
 import no.nav.dagpenger.model.fakta.faktum
 import no.nav.dagpenger.model.regel.har
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class JsonBuilderTest{
+internal class JsonBuilderTest {
 
     @Test
-    fun `a`(){
+    fun `a`() {
         val faktumNavnId = 1
         val faktum = FaktumNavn(faktumNavnId, "faktum").faktum<Boolean>()
 
