fa976ea6b560c538193651df58525b64d456bb22
Atle Nordland <atle.nordland@nav.no>
2020-09-21 10:49:26 +0200

    Laget enkel SøknadJsonBuilder
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- model/src/main/kotlin/no/nav/dagpenger/model/søknad/Søknad.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/søknad/Søknad.kt
@@ -1,9 +1,15 @@
 package no.nav.dagpenger.model.søknad
 
 import no.nav.dagpenger.model.subsumsjon.Subsumsjon
+import no.nav.dagpenger.model.visitor.SøknadVisitor
 import java.util.UUID
 
-class Søknad private constructor(uuid: UUID, private val seksjoner: List<Seksjon>) : Collection<Seksjon> by seksjoner {
+class Søknad private constructor(private val uuid: UUID, private val seksjoner: List<Seksjon>) : Collection<Seksjon> by seksjoner {
     constructor(vararg seksjoner: Seksjon) : this(UUID.randomUUID(), seksjoner.toList())
     infix fun nesteSeksjon(subsumsjon: Subsumsjon) = seksjoner.first { subsumsjon.nesteFakta() in it }
+
+    internal fun accept(visitor: SøknadVisitor) {
+        visitor.preVisit(this, uuid)
+        visitor.postVisit(this)
+    }
 }
