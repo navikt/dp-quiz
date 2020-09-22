1b25e52b7f529b647a682e9d18e5a57238dcbf25
Fred George <fredgeorge@acm.org>
2020-09-22 13:02:15 +0200

    Bug with missing parameter on dagpenger module change

--- dagpenger/src/main/kotlin/no/nav/dagpenger/søknad/Datamaskin.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/søknad/Datamaskin.kt
@@ -1,8 +1,9 @@
 package no.nav.dagpenger.søknad
 
+import no.nav.dagpenger.model.fakta.Rolle
 import no.nav.dagpenger.model.søknad.Seksjon
 import no.nav.dagpenger.model.søknad.Søknad
 import no.nav.dagpenger.regelverk.fødselsdato
 import no.nav.dagpenger.regelverk.ønsketDato
 
-val datamaskin = Søknad(Seksjon(ønsketDato, fødselsdato))
+val datamaskin = Søknad(Seksjon(Rolle.søker, ønsketDato, fødselsdato))
