267ead475cc88cece20a693eeda1331b93b10c27
André Roaldseth <andre.roaldseth@nav.no>
2020-09-24 13:48:09 +0200

    Fjern ubrukt post import

--- dagpenger/src/main/kotlin/no/nav/dagpenger/SøknadApi.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/SøknadApi.kt
@@ -9,7 +9,6 @@ import io.ktor.jackson.jackson
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.get
-import io.ktor.routing.post
 import io.ktor.routing.put
 import io.ktor.routing.route
 import io.ktor.routing.routing
