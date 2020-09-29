9fc83a49662e3e4f372ea3f26d990165bb43dcfa
Celine ten Dam <celine.ten.dam@nav.no>
2020-09-29 09:49:41 +0200

    ferdig med flere templates
    Co-authored-by: Fred George <fredgeorge@acm.org>
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>
    Co-authored-by: Marte Pedersen <marte.pedersen@nav.no>

--- model/src/test/kotlin/no/nav/dagpenger/model/unit/fakta/GenerertFaktumTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/unit/fakta/GenerertFaktumTest.kt
@@ -24,5 +24,24 @@ internal class GenerertFaktumTest {
         assertEquals("1.5", seksjon[5].id)
     }
 
+    @Test
+    fun `Flere templates`() {
+        val template1 = FaktumNavn(1, "template1").template(Boolean::class.java)
+        val template2 = FaktumNavn(2, "template2").template(Boolean::class.java)
+        val template3 = FaktumNavn(3, "template3").template(Boolean::class.java)
+        val generator = FaktumNavn(4, "generer").faktum(Int::class.java, template1, template2, template3)
+        val seksjon1 = Seksjon(Rolle.søker, template1, generator)
+        val seksjon2 = Seksjon(Rolle.søker, template2, template3)
+        val originalSize1 = seksjon1.size
+        val originalSize2 = seksjon2.size
+        generator.besvar(3)
+
+        assertEquals(3, seksjon1.size - originalSize1)
+        assertEquals(6, seksjon2.size - originalSize2)
+        assertEquals("1.1", seksjon1[1].id)
+        assertEquals("2.1", seksjon2[1].id)
+        assertEquals("3.3", seksjon2[7].id)
+    }
+
     private operator fun Seksjon.get(indeks: Int) = this.sortedBy { it.id }[indeks]
 }
