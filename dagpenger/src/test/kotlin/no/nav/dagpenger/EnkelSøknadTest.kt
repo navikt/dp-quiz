a3316e451c14b9dfcdec99c812354c0c7174cdaa
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2020-10-02 09:15:04 +0200

    test at vi får ny instans av faktum per søknad
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: kriise <kmriise@gmail.com>

--- dagpenger/src/test/kotlin/no/nav/dagpenger/EnkelSøknadTest.kt
+++ dagpenger/src/test/kotlin/no/nav/dagpenger/EnkelSøknadTest.kt
@@ -4,6 +4,9 @@ import no.nav.dagpenger.regelverk.inngangsvilkår
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import java.time.LocalDate
+import kotlin.test.assertEquals
+import kotlin.test.assertNotSame
 
 internal class EnkelSøknadTest {
     @Test
@@ -14,5 +17,13 @@ internal class EnkelSøknadTest {
         assertDoesNotThrow("Alle faktum i inngansvilkår må være til stede i søknaden") {
             inngangsvilkår.deepCopy(søknad)
         }
+    }
+
+    @Test
+    fun `at vi får ny instans av faktum for hver søknad`(){
+        val søknad1 = EnkelSøknad().søknad()
+        val søknad2 = EnkelSøknad().søknad()
+
+        assertNotSame(søknad1.finnFaktum<LocalDate>("1"), søknad2.finnFaktum<LocalDate>("1"))
     }
 }
