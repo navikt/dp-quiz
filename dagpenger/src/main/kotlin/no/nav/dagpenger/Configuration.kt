fb842b68ca60b7ed0e2406b64778c70063ce2bb8
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2020-10-19 11:53:26 +0200

    add kafka topic

--- dagpenger/src/main/kotlin/no/nav/dagpenger/Configuration.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/Configuration.kt
@@ -1,32 +1,23 @@
 package no.nav.dagpenger
 
-import com.natpryce.konfig.ConfigurationMap
 import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
 import com.natpryce.konfig.EnvironmentVariables
 import com.natpryce.konfig.Key
-import com.natpryce.konfig.getValue
-import com.natpryce.konfig.intType
 import com.natpryce.konfig.overriding
 import com.natpryce.konfig.stringType
 
 internal object Configuration {
-    private val port by intType
 
-    private val defaults = ConfigurationMap(
-        port to "8080"
-    )
-
-    private val config = systemProperties() overriding EnvironmentVariables() overriding defaults
+    private val config = systemProperties() overriding EnvironmentVariables()
 
     val rapidApplication: Map<String, String> = mapOf(
         "RAPID_APP_NAME" to "dp-quiz",
         "KAFKA_BOOTSTRAP_SERVERS" to config[Key("kafka.bootstrap.servers", stringType)],
         "KAFKA_CONSUMER_GROUP_ID" to "dp-quiz-v1",
-        "KAFKA_RAPID_TOPIC" to config[Key("kafka.topic", stringType)],
-        // "KAFKA_EXTRA_TOPIC" to config()[Key("extra.kafka.topics", stringType)],
+        "KAFKA_RAPID_TOPIC" to config.getOrElse(Key("kafka.topic", stringType), "privat-dagpenger-behov-v2"),
         "KAFKA_RESET_POLICY" to config[Key("kafka.reset.policy", stringType)],
         "NAV_TRUSTSTORE_PATH" to config[Key("nav.truststore.path", stringType)],
         "NAV_TRUSTSTORE_PASSWORD" to config[Key("nav.truststore.password", stringType)],
-        "HTTP_PORT" to config[Key("port", stringType)]
+        "HTTP_PORT" to config.getOrElse(Key("port", stringType), "8080")
     )
 }
