0d5fc1e5d80c017e80608a186a033cc5ae928e41
Fred George <fredgeorge@acm.org>
2020-10-23 13:51:37 +0200

    Removed duplicate faktum ID check in Søknad; done in the Fakta now

--- dagpenger/src/test/kotlin/no/nav/dagpenger/model/søknad/SøknadTest.kt
+++ dagpenger/src/test/kotlin/no/nav/dagpenger/model/søknad/SøknadTest.kt
@@ -3,23 +3,12 @@ package no.nav.dagpenger.model.søknad
 import no.nav.dagpenger.model.fakta.FaktumNavn
 import no.nav.dagpenger.model.fakta.Rolle
 import no.nav.dagpenger.model.fakta.faktum
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 internal class SøknadTest {
-
-    @Test @Disabled
-    fun `Fakta med dupliserte navn`() {
-        val navn = FaktumNavn(3, "a")
-        val faktum1 = navn.faktum(Boolean::class.java)
-        val faktum2 = navn.faktum(Boolean::class.java)
-        val seksjon1 = Seksjon("seksjon", Rolle.søker, faktum1)
-        val seksjon2 = Seksjon("seksjon", Rolle.søker, faktum2)
-        assertThrows<IllegalArgumentException> { Søknad(seksjon1, seksjon2) }
-    }
 
     @Test
     fun `Samme faktum kan være i flere seksjoner`() {
