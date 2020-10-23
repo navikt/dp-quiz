108cd091e6ab175f726329e9d4475e9f750da775
Geir André Lund <geir.andre.lund@nav.no>
2020-10-23 10:54:50 +0200

    Fikser HendelseMediator etter ny modell merge WIP - Trenger å bruke Versjon
    Co-authored-by: Knut Magne Riise <knut.magne.riise@nav.no>

--- dagpenger/src/main/kotlin/no/nav/dagpenger/EnkelSøknad.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/EnkelSøknad.kt
@@ -1,6 +1,5 @@
 package no.nav.dagpenger
 
-
 internal class EnkelSøknad : SøknadBygger {
     private var prototype = Prototype()
 
--- mediator/src/main/kotlin/HendelseMediator.kt
+++ mediator/src/main/kotlin/HendelseMediator.kt
@@ -1,7 +1,7 @@
 import db.Søknader
 import meldinger.model.ØnskerRettighetsavklaring
 import meldinger.model.ØnskerRettighetsavklaringMelding
-import no.nav.dagpenger.EnkelSøknad
+import no.nav.dagpenger.Prototype
 import no.nav.dagpenger.regelverk.inngangsvilkår
 import no.nav.helse.rapids_rivers.RapidsConnection
 
@@ -9,7 +9,7 @@ internal class HendelseMediator(private val søknader: Søknader, private val ra
     private val behovMediator = BehovMediator(rapidsConnection)
 
     fun behandle(melding: ØnskerRettighetsavklaringMelding, ønskerRettighetsavklaring: ØnskerRettighetsavklaring) {
-        val enkelSøknad = EnkelSøknad().søknad()
+        val enkelSøknad = Prototype().søknad(fnr = "TODO!") // todo: Replace by Versjon
         søknader.nySøknad(enkelSøknad)
         val nesteSeksjon = enkelSøknad.nesteSeksjon(inngangsvilkår.deepCopy(enkelSøknad))
         behovMediator.håndter(nesteSeksjon)
