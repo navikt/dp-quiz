acdce681d3dcad2112538462918038a30c985af8
Giao The Cung <giaothe@gmail.com>
2020-10-26 16:54:20 +0100

    Fiks bygget.

--- dagpenger/build.gradle.kts
+++ dagpenger/build.gradle.kts
@@ -18,6 +18,6 @@ dependencies {
     implementation(Kotlin.Logging.kotlinLogging)
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("net.logstash.logback:logstash-logback-encoder:6.4")
-    implementation("com.github.navikt:rapids-and-rivers:1.74ae9cb")
+    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
     testImplementation(Ktor.ktorTest)
 }
---
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/App.kt
@@ -0,0 +1,7 @@
+package no.nav.dagpenger
+
+class App {
+    fun main() {
+        ApplicationBuilder().start()
+    }
+}
