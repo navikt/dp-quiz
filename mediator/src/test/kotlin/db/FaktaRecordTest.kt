c5ba7588337586e53b1d4e32506c2bfc5f492985
Fred George <fredgeorge@acm.org>
2020-10-29 08:14:12 +0100

    Refactored the FaktaRecordTest, teasing out some common code, and speculating on additional needed tests

--- mediator/src/test/kotlin/db/FaktaRecordTest.kt
+++ mediator/src/test/kotlin/db/FaktaRecordTest.kt
@@ -8,6 +8,7 @@ import kotliquery.sessionOf
 import kotliquery.using
 import no.nav.dagpenger.model.fakta.Inntekt.Companion.årlig
 import no.nav.dagpenger.model.fakta.Rolle
+import no.nav.dagpenger.model.søknad.Søknad
 import no.nav.dagpenger.model.søknad.Versjon
 import no.nav.helse.serde.assertDeepEquals
 import org.junit.jupiter.api.Test
@@ -18,43 +19,65 @@ internal class FaktaRecordTest {
     companion object {
         internal const val UNG_PERSON_FNR_2018 = "12020052345"
     }
+    private lateinit var originalSøknad: Søknad
+    private lateinit var rehydrertSøknad: Søknad
+    private lateinit var faktaRecord: FaktaRecord
 
     @Test
     fun `ny søknad`() {
         Postgres.withMigratedDb {
-            FaktumTable(prototypeFakta, 1)
-            val originalSøknad = FaktaRecord().ny(UNG_PERSON_FNR_2018, Versjon.Type.Web)
+            byggOriginalSøknad()
+
             assertRecordCount(1, "fakta")
             assertRecordCount(21, "faktum_verdi")
             FaktaRecord().ny(UNG_PERSON_FNR_2018, Versjon.Type.Web)
             assertRecordCount(2, "fakta")
             assertRecordCount(42, "faktum_verdi")
-
-            val uuid = FaktaRecord().opprettede(UNG_PERSON_FNR_2018).toSortedMap().values.first()
-            val rehydrertSøknad = FaktaRecord().hent(uuid, Versjon.Type.Web)
-
-            // assertDeepEquals(originalSøknad, rehydrertSøknad)
+            hentFørstFakta()
         }
     }
 
     @Test
     fun `lagring og henting av fakta`() {
         Postgres.withMigratedDb {
-            FaktumTable(prototypeFakta, 1)
-            val faktaRecord = FaktaRecord()
-            val originalSøknad = faktaRecord.ny(UNG_PERSON_FNR_2018, Versjon.Type.Web)
+            byggOriginalSøknad()
 
             originalSøknad.ja(1).besvar(true, Rolle.søker)
             originalSøknad.dato(2).besvar(LocalDate.now(), Rolle.søker)
             originalSøknad.inntekt(6).besvar(10000.årlig, Rolle.søker)
             originalSøknad.heltall(16).besvar(123, Rolle.søker)
 
-            faktaRecord.lagre(originalSøknad.fakta)
-
-            val uuid = FaktaRecord().opprettede(UNG_PERSON_FNR_2018).toSortedMap().values.first()
-            val rehydrertSøknad = FaktaRecord().hent(uuid, Versjon.Type.Web)
-            assertDeepEquals(originalSøknad, rehydrertSøknad)
+            hentFørstFakta()
         }
+    }
+
+    @Test
+    fun `dependent faktum reset`() {
+    }
+
+    @Test
+    fun `expanded template faktum`() {
+    }
+
+    @Test
+    fun `reduced template faktum`() {
+    }
+
+    @Test
+    fun `utledet faktum with value`() {
+    }
+
+    private fun hentFørstFakta() {
+        faktaRecord.lagre(originalSøknad.fakta)
+        val uuid = FaktaRecord().opprettede(UNG_PERSON_FNR_2018).toSortedMap().values.first()
+        rehydrertSøknad = FaktaRecord().hent(uuid, Versjon.Type.Web)
+        assertDeepEquals(originalSøknad, rehydrertSøknad)
+    }
+
+    private fun byggOriginalSøknad() {
+        FaktumTable(prototypeFakta, 1)
+        faktaRecord = FaktaRecord()
+        originalSøknad = faktaRecord.ny(UNG_PERSON_FNR_2018, Versjon.Type.Web)
     }
 
     private fun assertRecordCount(recordCount: Int, table: String) {
