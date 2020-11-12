4ded58c50afd509ea621e74cd2daca4b73b2314a
Celine ten Dam <celine.ten.dam@nav.no>
2020-11-12 09:50:51 +0100

    La til test for sending av subsumsjoner til saksbehandler
    Co-authored-by: Fred George <fredgeorge@acm.org>

--- model/src/main/kotlin/no/nav/dagpenger/model/marshalling/SaksbehandlerJsonBuilder.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/marshalling/SaksbehandlerJsonBuilder.kt
@@ -13,6 +13,8 @@ import no.nav.dagpenger.model.faktum.Rolle
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.faktum.UtledetFaktum
 import no.nav.dagpenger.model.faktum.ValgFaktum
+import no.nav.dagpenger.model.regel.Regel
+import no.nav.dagpenger.model.subsumsjon.EnkelSubsumsjon
 import no.nav.dagpenger.model.subsumsjon.GodkjenningsSubsumsjon
 import no.nav.dagpenger.model.visitor.FaktagrupperVisitor
 import java.time.LocalDate
@@ -27,7 +29,7 @@ class SaksbehandlerJsonBuilder(
     private val mapper = ObjectMapper()
     private val root: ObjectNode = mapper.createObjectNode()
     private val faktaNode = mapper.createArrayNode()
-    private lateinit var subsumsjonRoot: ObjectNode
+    private val subsumsjonRoot = mapper.createArrayNode()
     private var ignore = true
     private var iValg = false
     private val faktumIder = mutableSetOf<String>()
@@ -39,9 +41,7 @@ class SaksbehandlerJsonBuilder(
         fakta.rootSubsumsjon.mulige().accept(this)
     }
 
-    fun resultat() = root.also {
-        // root.set("subsumsjon", subsumsjonRoot)
-    }
+    fun resultat() = root
 
     override fun preVisit(søknad: Søknad, fnr: String, versjonId: Int, uuid: UUID) {
         root.put("@event_name", "oppgave")
@@ -52,6 +52,7 @@ class SaksbehandlerJsonBuilder(
         root.put("seksjon_navn", seksjonNavn)
         root.put("indeks", indeks)
         root.set("fakta", faktaNode)
+        root.set("subsumsjoner", subsumsjonRoot)
     }
 
     override fun preVisit(seksjon: Seksjon, rolle: Rolle, fakta: Set<Faktum<*>>, indeks: Int) {
@@ -151,11 +152,24 @@ class SaksbehandlerJsonBuilder(
     }
 
     override fun preVisit(subsumsjon: GodkjenningsSubsumsjon, resultat: Boolean?) {
-        subsumsjonNoder.add(0, mapper.createObjectNode())
+        subsumsjonNoder.add(0, subsumsjonRoot.addObject())
     }
 
     override fun postVisit(subsumsjon: GodkjenningsSubsumsjon, resultat: Boolean?) {
-        subsumsjonRoot = subsumsjonNoder.removeAt(0)
+        subsumsjonNoder.removeAt(0)
+    }
+
+    override fun preVisit(subsumsjon: EnkelSubsumsjon, regel: Regel, fakta: Set<Faktum<*>>, resultat: Boolean?) {
+        subsumsjonRoot.addObject().also { subsumsjonNode ->
+            subsumsjonNoder.add(0, subsumsjonNode)
+            subsumsjonNode.put("resultat", resultat)
+            subsumsjonNode.put("navn", subsumsjon.navn)
+            subsumsjonNode.put("type", "Enkel subsumsjon")
+        }
+    }
+
+    override fun postVisit(subsumsjon: EnkelSubsumsjon, regel: Regel, fakta: Set<Faktum<*>>, resultat: Boolean?) {
+        subsumsjonNoder.removeAt(0)
     }
 
     private fun <R : Comparable<R>> lagFaktumNode(id: String, roller: Set<Rolle> = emptySet(), svar: R? = null) {
--- model/src/test/kotlin/no/nav/dagpenger/model/unit/marshalling/SaksbehandlerSubsumsjonTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/unit/marshalling/SaksbehandlerSubsumsjonTest.kt
@@ -7,47 +7,38 @@ import no.nav.dagpenger.model.faktagrupper.Versjon
 import no.nav.dagpenger.model.faktagrupper.Versjon.FaktagrupperType.Web
 import no.nav.dagpenger.model.faktum.Rolle
 import no.nav.dagpenger.model.faktum.Søknad
-import no.nav.dagpenger.model.helpers.NyttEksempel
 import no.nav.dagpenger.model.marshalling.SaksbehandlerJsonBuilder
 import no.nav.dagpenger.model.regel.er
 import no.nav.dagpenger.model.regel.gyldigGodkjentAv
 import no.nav.dagpenger.model.regel.ugyldigGodkjentAv
+import no.nav.dagpenger.model.subsumsjon.Subsumsjon
 import no.nav.dagpenger.model.subsumsjon.så
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import java.util.UUID
 
-internal class SaksbehandlerJsonBuilderTest {
-    @Test
-    fun `byggger oppgave event`() {
-        val prototypeSøknad = Søknad(
+internal class SaksbehandlerSubsumsjonTest {
+    companion object {
+        private val prototypeSøknad = Søknad(
             ja nei "f1" id 1,
             ja nei "f2" id 2 avhengerAv 1,
             ja nei "f3" id 3,
             ja nei "f4" id 4 avhengerAv 3,
         )
 
-        val prototypeSubsumsjon =
+        private var versjonId = 1
+    }
+
+    @Test
+    fun `byggger oppgave event`() {
+
+        val fakta = faktagrupper(
             prototypeSøknad.ja(1) er true gyldigGodkjentAv prototypeSøknad.ja(2) så
                 (prototypeSøknad.ja(3) er true ugyldigGodkjentAv prototypeSøknad.ja(4))
-
-        val prototypeFaktagrupper = Faktagrupper(
-            prototypeSøknad,
-            Seksjon("søker", Rolle.søker, prototypeSøknad.ja(1), prototypeSøknad.ja(3)),
-            Seksjon("saksbehandler2", Rolle.saksbehandler, prototypeSøknad.ja(2)),
-            Seksjon("saksbehandler4", Rolle.saksbehandler, prototypeSøknad.ja(4)),
-            rootSubsumsjon = prototypeSubsumsjon
         )
 
-        Versjon(
-            1,
-            prototypeSøknad,
-            prototypeSubsumsjon,
-            mapOf(Web to prototypeFaktagrupper)
-        )
-
-        val fakta = Versjon.siste.faktagrupper(fnr = "12345678910", Web)
         val json = SaksbehandlerJsonBuilder(fakta, "saksbehandler2").resultat()
 
         assertEquals("oppgave", json["@event_name"].asText())
@@ -67,24 +58,31 @@ internal class SaksbehandlerJsonBuilderTest {
     }
 
     @Test
-    fun `Komplekse seksjoner`() {
-        assertSeksjonSize(8, "seksjon8")
-        assertSeksjonSize(5, "seksjon4")
-        assertSeksjonSize(5, "seksjon2")
+    fun test() {
+        val fakta = faktagrupper(prototypeSøknad.ja(1) er true)
+        fakta.ja(1).besvar(true)
+        val json = SaksbehandlerJsonBuilder(fakta, "saksbehandler2").resultat()
+
+        assertEquals(1, json["subsumsjoner"].size())
+        assertTrue(json["subsumsjoner"][0]["resultat"].asBoolean())
     }
 
-    @Test
-    fun `Genererte seksjoner kan bli sendt`() {
-        val fakta = NyttEksempel().faktagrupper
-        fakta.heltall(15).besvar(3)
-        var json = SaksbehandlerJsonBuilder(fakta, "seksjon8").resultat()
-        assertEquals(11, json["fakta"].size())
-        json = SaksbehandlerJsonBuilder(fakta, "seksjon7", 1).resultat()
-        assertEquals(1, json["fakta"].size())
-    }
+    private fun faktagrupper(prototypeSubsumsjon: Subsumsjon): Faktagrupper {
+        val prototypeFaktagrupper = Faktagrupper(
+            prototypeSøknad,
+            Seksjon("søker", Rolle.søker, prototypeSøknad.ja(1), prototypeSøknad.ja(3)),
+            Seksjon("saksbehandler2", Rolle.saksbehandler, prototypeSøknad.ja(2)),
+            Seksjon("saksbehandler4", Rolle.saksbehandler, prototypeSøknad.ja(4)),
+            rootSubsumsjon = prototypeSubsumsjon
+        )
 
-    private fun assertSeksjonSize(expected: Int, seksjonNavn: String) {
-        val json = SaksbehandlerJsonBuilder(NyttEksempel().faktagrupper, seksjonNavn).resultat()
-        assertEquals(expected, json["fakta"].size())
+        Versjon(
+            versjonId++,
+            prototypeSøknad,
+            prototypeSubsumsjon,
+            mapOf(Web to prototypeFaktagrupper)
+        )
+
+        return Versjon.siste.faktagrupper("12345678910", Web)
     }
 }
