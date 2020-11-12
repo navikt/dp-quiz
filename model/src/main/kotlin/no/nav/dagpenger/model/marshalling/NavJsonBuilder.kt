40d44499971ca49cec220d6ef786ac024a8b01f0
Atle Nordland <atle.nordland@nav.no>
2020-11-12 10:57:02 +0100

    WIP: NavJsonBuilder
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

---
+++ model/src/main/kotlin/no/nav/dagpenger/model/marshalling/NavJsonBuilder.kt
@@ -0,0 +1,153 @@
+package no.nav.dagpenger.model.marshalling
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
+import no.nav.dagpenger.model.faktagrupper.Faktagrupper
+import no.nav.dagpenger.model.faktagrupper.Seksjon
+import no.nav.dagpenger.model.faktum.Dokument
+import no.nav.dagpenger.model.faktum.Faktum
+import no.nav.dagpenger.model.faktum.GrunnleggendeFaktum
+import no.nav.dagpenger.model.faktum.Inntekt
+import no.nav.dagpenger.model.faktum.Rolle
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.visitor.FaktagrupperVisitor
+import java.time.LocalDate
+import java.util.UUID
+
+class NavJsonBuilder(fakta: Faktagrupper) : FaktagrupperVisitor {
+    private val mapper = ObjectMapper()
+    private val root: ObjectNode = mapper.createObjectNode()
+    private val faktaNode = mapper.createArrayNode()
+    private var ignore = true
+    private val faktumIder = mutableSetOf<String>()
+
+    init {
+        // fakta.søknad.accept(this)
+        fakta.accept(this)
+    }
+
+    fun resultat() = root
+
+    override fun preVisit(søknad: Søknad, fnr: String, versjonId: Int, uuid: UUID) {
+        root.put("@event_name", "behov")
+        root.put("@event_name", "behov")
+        root.put("fnr", fnr)
+        root.put("soknad_uuid", "$uuid")
+        root.set("fakta", faktaNode)
+    }
+
+    override fun preVisit(seksjon: Seksjon, rolle: Rolle, fakta: Set<Faktum<*>>, indeks: Int) {
+        ignore = rolle != Rolle.nav
+    }
+
+    override fun <R : Comparable<R>> visit(
+        faktum: GrunnleggendeFaktum<R>,
+        tilstand: Faktum.FaktumTilstand,
+        id: String,
+        avhengigeFakta: Set<Faktum<*>>,
+        avhengerAvFakta: Set<Faktum<*>>,
+        roller: Set<Rolle>,
+        clazz: Class<R>
+    ) {
+        if (avhengerAvFakta.all { it.erBesvart() })
+            lagFaktumNode<R>(id, roller)
+    }
+
+    /*
+    override fun <R : Comparable<R>> visit(
+            faktum: GrunnleggendeFaktum<R>,
+            tilstand: Faktum.FaktumTilstand,
+            id: String,
+            avhengigeFakta: Set<Faktum<*>>,
+            avhengerAvFakta: Set<Faktum<*>>,
+            roller: Set<Rolle>,
+            clazz: Class<R>,
+            svar: R
+    ) {
+        //lagFaktumNode(id, roller, svar)
+    }
+
+    override fun <R : Comparable<R>> preVisit(
+            faktum: UtledetFaktum<R>,
+            id: String,
+            avhengigeFakta: Set<Faktum<*>>,
+            avhengerAvFakta: Set<Faktum<*>>,
+            children: Set<Faktum<*>>,
+            clazz: Class<R>,
+            regel: FaktaRegel<R>
+    ) {
+        lagFaktumNode<R>(id)
+    }
+
+    override fun <R : Comparable<R>> preVisit(
+            faktum: UtledetFaktum<R>,
+            id: String,
+            avhengigeFakta: Set<Faktum<*>>,
+            avhengerAvFakta: Set<Faktum<*>>,
+            children: Set<Faktum<*>>,
+            clazz: Class<R>,
+            regel: FaktaRegel<R>,
+            svar: R
+    ) {
+        //lagFaktumNode(id, svar = svar)
+    }
+
+    override fun preVisit(
+            faktum: ValgFaktum,
+            id: String,
+            avhengigeFakta: Set<Faktum<*>>,
+            avhengerAvFakta: Set<Faktum<*>>,
+            underordnedeJa: Set<Faktum<Boolean>>,
+            underordnedeNei: Set<Faktum<Boolean>>,
+            clazz: Class<Boolean>
+    ) {
+        lagFaktumNode<Boolean>(id)
+    }
+
+    override fun preVisit(
+            faktum: ValgFaktum,
+            id: String,
+            avhengigeFakta: Set<Faktum<*>>,
+            avhengerAvFakta: Set<Faktum<*>>,
+            underordnedeJa: Set<Faktum<Boolean>>,
+            underordnedeNei: Set<Faktum<Boolean>>,
+            clazz: Class<Boolean>,
+            svar: Boolean
+    ) {
+        //lagFaktumNode(id, svar = svar)
+    }
+
+     */
+
+    private fun <R : Comparable<R>> lagFaktumNode(id: String, roller: Set<Rolle> = emptySet(), svar: R? = null) {
+        if (ignore) return
+        if (id in faktumIder) return
+        faktaNode.addObject().also { faktumNode ->
+            faktumNode.put("id", id)
+            faktumNode.set("roller", mapper.valueToTree(roller.map { it.name }))
+            svar?.also { faktumNode.putR(it) }
+        }
+        faktumIder.add(id)
+    }
+
+    private fun <R : Comparable<R>> ObjectNode.putR(svar: R) {
+        when (svar) {
+            is Boolean -> this.put("svar", svar)
+            is Int -> this.put("svar", svar)
+            is Double -> this.put("svar", svar)
+            is String -> this.put("svar", svar)
+            is LocalDate -> this.put("svar", svar.toString())
+            is Dokument -> this.set(
+                "svar",
+                svar.reflection { lastOppTidsstempel, url ->
+                    mapper.createObjectNode().also {
+                        it.put("lastOppTidsstempel", lastOppTidsstempel.toString())
+                        it.put("url", url)
+                    }
+                }
+            )
+            is Inntekt -> this.put("svar", svar.reflection { årlig, _, _, _ -> årlig })
+            else -> throw IllegalArgumentException("Ukjent datatype")
+        }
+    }
+}
---
+++ model/src/test/kotlin/no/nav/dagpenger/model/unit/marshalling/NavJsonBuilderTest.kt
@@ -0,0 +1,60 @@
+package no.nav.dagpenger.model.unit.marshalling
+
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.ja
+import no.nav.dagpenger.model.faktagrupper.Faktagrupper
+import no.nav.dagpenger.model.faktagrupper.Seksjon
+import no.nav.dagpenger.model.faktagrupper.Versjon
+import no.nav.dagpenger.model.faktum.Rolle
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.marshalling.NavJsonBuilder
+import no.nav.dagpenger.model.regel.er
+import no.nav.dagpenger.model.subsumsjon.alle
+import no.nav.dagpenger.model.subsumsjon.så
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NavJsonBuilderTest {
+    @Test
+    fun `bygger behov event`() {
+        val prototypeSøknad = Søknad(
+                ja nei "f1" id 1,
+                ja nei "f1" id 2 avhengerAv 1,
+                ja nei "f3" id 3,
+                ja nei "f3" id 4 avhengerAv 3,
+        )
+
+        val prototypeSubsumsjon =
+                prototypeSøknad.ja(1) er true så
+                        "alle".alle (
+                            prototypeSøknad.ja(2) er true,
+                            prototypeSøknad.ja(3) er true,
+                            prototypeSøknad.ja(4) er true
+                        )
+
+        val søkerSeksjon = Seksjon("seksjon søker", Rolle.søker, prototypeSøknad.ja(1))
+        val navSeksjon = Seksjon("seksjon nav", Rolle.nav, prototypeSøknad.ja(2), prototypeSøknad.ja(3), prototypeSøknad.ja(4))
+
+        val prototypeFaktagrupper = Faktagrupper(
+                prototypeSøknad,
+                søkerSeksjon,
+                navSeksjon,
+                rootSubsumsjon = prototypeSubsumsjon
+        )
+
+        Versjon(
+                1,
+                prototypeSøknad,
+                prototypeSubsumsjon,
+                mapOf(Versjon.FaktagrupperType.Web to prototypeFaktagrupper)
+        )
+
+        prototypeSøknad.ja(1).besvar(true)
+
+        val fakta = Versjon.siste.faktagrupper(fnr = "12345678910", Versjon.FaktagrupperType.Web)
+        val json = NavJsonBuilder(fakta).resultat()
+
+        assertEquals("behov", json["@event_name"].asText())
+        assertEquals("12345678910", json["fnr"].asText())
+        //assertEquals(1, json["fakta"].size())
+    }
+}
