4530478c7331584575742c937fc54a4ce89c2751
Atle Nordland <atle.nordland@nav.no>
2020-11-12 14:33:26 +0100

    NavJsonBuilder handerter faktum med rolle nav, og sender ut liste med behov
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

---
+++ model/src/main/kotlin/no/nav/dagpenger/model/marshalling/FaktumBehov.kt
@@ -0,0 +1,15 @@
+package no.nav.dagpenger.model.marshalling
+
+// Forstår hvordan en kan gå fra faktum til behov for en spesifikk versjon
+internal class FaktumBehov(versjon: Int, private val delegate: Map<Int, String>) : Map<Int, String> by delegate {
+    override operator fun get(key: Int): String = delegate[key] ?: throw IllegalArgumentException("Ukjent faktum id $key")
+
+    init {
+        versjoner[versjon] = this
+    }
+
+    companion object {
+        private val versjoner = mutableMapOf<Int, FaktumBehov>()
+        fun id(versjonId: Int) = requireNotNull(versjoner[versjonId]) { "Vet ikke om versjon $versjonId" }
+    }
+}
