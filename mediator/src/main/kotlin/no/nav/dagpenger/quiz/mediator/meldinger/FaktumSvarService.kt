4150fc3109ef4b064626f677f5cb87460ed75434
marte pedersen <marte.pedersen@nav.no>
2020-11-16 12:25:27 +0100

    fjerna duplisert kode
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/FaktumSvarService.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/FaktumSvarService.kt
@@ -4,34 +4,32 @@ import com.fasterxml.jackson.databind.JsonNode
 import no.nav.dagpenger.model.faktagrupper.Faktagrupper
 import no.nav.dagpenger.model.faktagrupper.Versjon
 import no.nav.dagpenger.model.faktum.Dokument
-import no.nav.dagpenger.model.faktum.Inntekt
 import no.nav.dagpenger.model.faktum.Inntekt.Companion.årlig
 import no.nav.dagpenger.quiz.mediator.db.SøknadPersistence
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.asLocalDate
 import no.nav.helse.rapids_rivers.asLocalDateTime
-import java.time.LocalDate
 import java.util.UUID
 
 internal class FaktumSvarService(
-    private val søknadPersistence: SøknadPersistence,
-    private val behovMediator: BehovMediator,
-    rapidsConnection: RapidsConnection
+        private val søknadPersistence: SøknadPersistence,
+        private val behovMediator: BehovMediator,
+        rapidsConnection: RapidsConnection
 ) :
-    HendelseService(rapidsConnection) {
+        HendelseService(rapidsConnection) {
 
     override val eventName = "faktum_svar"
     override val riverName = "Faktum svar"
 
     override fun validate(packet: JsonMessage) {
         packet.requireKey(
-            "fnr",
-            "opprettet",
-            "faktumId",
-            "svar",
-            "søknadUuid",
-            "clazz",
+                "fnr",
+                "opprettet",
+                "faktumId",
+                "svar",
+                "søknadUuid",
+                "clazz",
         )
     }
 
@@ -39,35 +37,27 @@ internal class FaktumSvarService(
         val fnr = packet["fnr"].asText()
         val søknadUuid = UUID.fromString(packet["søknadUuid"].asText())
         val faktumId = packet["faktumId"].asInt()
-        val typedSvar = typedSvar(packet["svar"], packet["clazz"].asText())
+        val svar = packet["svar"]
+        val clazz = packet["clazz"].asText()
 
         søknadPersistence.hent(søknadUuid, Versjon.FaktagrupperType.Web).also { faktagrupper ->
-            besvar(faktagrupper, faktumId, typedSvar)
+            besvar(faktagrupper, faktumId, svar, clazz)
             søknadPersistence.lagre(faktagrupper.søknad)
             faktagrupper.nesteSeksjoner()
-                .onEach { seksjon ->
-                    behovMediator.håndter(seksjon, fnr, faktagrupper.søknad.uuid)
-                }
+                    .onEach { seksjon ->
+                        behovMediator.håndter(seksjon, fnr, faktagrupper.søknad.uuid)
+                    }
             // .also { if (it.isEmpty()) behandleFerdigResultat() }
         }
     }
 
-    private fun typedSvar(svar: JsonNode, clazz: String) = when (clazz) {
-        "boolean" -> svar.asBoolean()
-        "heltall" -> svar.asInt()
-        "dato" -> svar.asLocalDate()
-        "inntekt" -> svar.asDouble().årlig
-        "dokument" -> Dokument(svar["lastOppTidsstempel"].asLocalDateTime(), svar["url"].asText())
-        else -> throw IllegalArgumentException("Ukjent faktum type: $clazz")
-    }
-
-    private fun besvar(faktagrupper: Faktagrupper, faktumId: Int, svar: Any) {
-        when (svar) {
-            is Boolean -> faktagrupper.ja(faktumId).besvar(svar)
-            is Int -> faktagrupper.heltall(faktumId).besvar(svar)
-            is LocalDate -> faktagrupper.dato(faktumId).besvar(svar)
-            is Inntekt -> faktagrupper.inntekt(faktumId).besvar(svar)
-            is Dokument -> faktagrupper.dokument(faktumId).besvar(svar)
+    private fun besvar(faktagrupper: Faktagrupper, faktumId: Int, svar: JsonNode, clazz: String) {
+        when (clazz) {
+            "boolean" -> faktagrupper.ja(faktumId).besvar(svar.asBoolean())
+            "heltall" -> faktagrupper.heltall(faktumId).besvar(svar.asInt())
+            "dato" -> faktagrupper.dato(faktumId).besvar(svar.asLocalDate())
+            "inntekt" -> faktagrupper.inntekt(faktumId).besvar(svar.asDouble().årlig)
+            "dokument" -> faktagrupper.dokument(faktumId).besvar(Dokument(svar["lastOppTidsstempel"].asLocalDateTime(), svar["url"].asText()))
             else -> throw IllegalArgumentException("Ukjent svar-type: ${svar::class.java}")
         }
     }
