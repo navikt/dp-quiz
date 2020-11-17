52c36931e1e5c5400f270f5dc7a991fc6ecf6e16
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2020-11-17 11:48:54 +0100

    code cleanup
    Co-authored-by: Geir A. Lund <geir.andre.lund@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/db/SøknadRecord.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/db/SøknadRecord.kt
@@ -150,7 +150,7 @@ class SøknadRecord : SøknadPersistence {
 
     override fun lagre(søknad: Søknad): Boolean {
         val nyeSvar = svarMap(søknad)
-        tilbakestillFakta(søknad, nyeSvar)
+        slettDødeFakta(søknad, nyeSvar)
         originalSvar.filterNot { (id, svar) -> nyeSvar[id] == svar }.forEach { (id, svar) ->
             val (rootId, indeks) = søknad.id(id).reflection { rootId, indeks -> rootId to indeks }
 
@@ -171,24 +171,31 @@ class SøknadRecord : SøknadPersistence {
         return true
     }
 
-    private fun tilbakestillFakta(søknad: Søknad, nyeSvar: Map<String, Any?>) {
-        originalSvar.keys.toSet().subtract(nyeSvar.keys.toSet()).forEach { id ->
-            val (rootId, indeks) = FaktumId(id).reflection { rootId, indeks -> Pair(rootId, indeks) }
-            using(sessionOf(dataSource)) { session ->
-                session.run(arkiverFaktum(søknad = søknad, rootId = rootId, indeks = indeks))
-                session.run(slettDødeFaktum(søknad = søknad, rootId, indeks))
-                originalSvar.remove(id)
+    private fun slettDødeFakta(søknad: Søknad, nyeSvar: Map<String, Any?>) {
+        originalSvar.keys.toSet()
+            .subtract(nyeSvar.keys.toSet())
+            .map { FaktumId(it).reflection { rootId, indeks -> Triple(rootId, indeks, it) } }
+            .forEach { (rootId, indeks, id) ->
+                using(sessionOf(dataSource)) { session ->
+                    session.run(arkiverFaktum(søknad = søknad, rootId = rootId, indeks = indeks))
+                    session.run(slettDødeFaktum(søknad = søknad, rootId, indeks))
+                    originalSvar.remove(id)
+                }
             }
-        }
     }
 
     private fun slettDødeFaktum(søknad: Søknad, rootId: Int, indeks: Int) = queryOf(
         //language=PostgreSQL
         """
               DELETE FROM faktum_verdi
-              WHERE id IN (SELECT faktum_verdi.id as faktum_id FROM soknad, faktum_verdi, faktum
-              WHERE faktum_verdi.soknad_id = soknad.id AND faktum_verdi.faktum_id = faktum.id AND soknad.uuid = ? AND faktum.root_id = ? 
-                    AND faktum_verdi.indeks = ?)
+              WHERE id IN 
+                (SELECT faktum_verdi.id as faktum_id FROM soknad, faktum_verdi, faktum
+                  WHERE faktum_verdi.soknad_id = soknad.id 
+                        AND faktum_verdi.faktum_id = faktum.id 
+                        AND soknad.uuid = ? 
+                        AND faktum.root_id = ? 
+                        AND faktum_verdi.indeks = ?
+                )
         """.trimIndent(),
         søknad.uuid,
         rootId,
