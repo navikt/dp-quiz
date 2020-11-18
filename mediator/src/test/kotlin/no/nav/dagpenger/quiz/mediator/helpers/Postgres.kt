8f7ce2e5881d10d723db14aa57009a4871cc9b39
Giao The Cung <giaothe@gmail.com>
2020-11-18 13:36:01 +0100

    Use 11.2 for now, 12 fails randomly
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/Postgres.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/Postgres.kt
@@ -7,7 +7,7 @@ import org.testcontainers.containers.PostgreSQLContainer.POSTGRESQL_PORT
 internal object Postgres {
 
     val instance by lazy {
-        PostgreSQLContainer<Nothing>("postgres:12").apply {
+        PostgreSQLContainer<Nothing>("postgres:11.2").apply {
             start()
         }
     }
