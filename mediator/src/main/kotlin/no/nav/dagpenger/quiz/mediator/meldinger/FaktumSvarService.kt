44acb256017da7a04ea44f87b495e015b38e4e98
Atle Nordland <atle.nordland@nav.no>
2020-11-27 09:30:32 +0100

    Debugging: logg packet on error
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: marte pedersen <marte.pedersen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/FaktumSvarService.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/FaktumSvarService.kt
@@ -14,6 +14,7 @@ import no.nav.helse.rapids_rivers.asLocalDateTime
 import java.util.UUID
 
 private val log = KotlinLogging.logger {}
+private val sikkerlogg = KotlinLogging.logger("tjenestekall")
 
 internal class FaktumSvarService(
     private val søknadPersistence: SøknadPersistence,
@@ -61,6 +62,9 @@ internal class FaktumSvarService(
         } catch (e: Exception) {
             log.error(e) {
                 "feil ved faktum svar: ${e.message}"
+            }
+            sikkerlogg.error(e) {
+                "feil ved faktum svar: ${e.message}, packet: ${packet.toJson()}"
             }
         }
     }
