b17dac74857b6f6a099f1933a7ded20a5b0a39ef
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2020-11-30 09:59:49 +0100

    Code cleanup: fjern enum faktum test fra factory test
    Co-authored-by: Geir A. Lund <geir.andre.lund@nav.no>

--- model/src/test/kotlin/no/nav/dagpenger/model/unit/factory/BaseFaktumFactoryTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/unit/factory/BaseFaktumFactoryTest.kt
@@ -14,8 +14,6 @@ import no.nav.dagpenger.model.faktum.Inntekt.Companion.årlig
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.helpers.januar
 import no.nav.dagpenger.model.helpers.testSøknadprosess
-import no.nav.dagpenger.model.unit.factory.BaseFaktumFactoryTest.Companion.Valg.A
-import no.nav.dagpenger.model.unit.factory.BaseFaktumFactoryTest.Companion.Valg.C
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -26,14 +24,6 @@ import kotlin.test.assertTrue
 
 internal class BaseFaktumFactoryTest {
 
-    companion object {
-        internal enum class Valg { A, B, C }
-    }
-
-    private object valg { infix fun faktum(navn: String) = BaseFaktumFactory(Valg::class.java, navn) }
-
-    // private object choices { fun <T> faktum(navn: String, enum: Enum<T>) = BaseFaktumFactory(enum::class.java, navn) }
-
     @Test
     fun boolean() {
         (ja nei "boolean" id 3).also { factory ->
@@ -42,7 +32,8 @@ internal class BaseFaktumFactoryTest {
         }
     }
 
-    @Test fun `boolean factory faktum`() {
+    @Test
+    fun `boolean factory faktum`() {
         val søknadprosess = Søknad(99, ja nei "boolean" id 3).testSøknadprosess()
         val faktum = søknadprosess ja 3
         assertFalse(faktum.erBesvart())
@@ -55,7 +46,8 @@ internal class BaseFaktumFactoryTest {
         assertFalse(faktum.svar())
     }
 
-    @Test fun `heltall factory faktum`() {
+    @Test
+    fun `heltall factory faktum`() {
         val søknadprosess = Søknad(98, heltall faktum "heltall" id 3).testSøknadprosess()
         val faktum = søknadprosess heltall 3
         assertFalse(faktum.erBesvart())
@@ -68,7 +60,8 @@ internal class BaseFaktumFactoryTest {
         assertEquals(3, faktum.svar())
     }
 
-    @Test fun `Inntekt factory faktum`() {
+    @Test
+    fun `Inntekt factory faktum`() {
         val søknadprosess = Søknad(97, inntekt faktum "inntekt" id 3).testSøknadprosess()
         val faktum = søknadprosess inntekt 3
         assertFalse(faktum.erBesvart())
@@ -81,7 +74,8 @@ internal class BaseFaktumFactoryTest {
         assertEquals(24.årlig, faktum.svar())
     }
 
-    @Test fun `Dato factory faktum`() {
+    @Test
+    fun `Dato factory faktum`() {
         val søknadprosess = Søknad(96, dato faktum "dato" id 3).testSøknadprosess()
         val faktum = søknadprosess dato 3
         assertFalse(faktum.erBesvart())
@@ -94,7 +88,8 @@ internal class BaseFaktumFactoryTest {
         assertEquals(2.januar(2020), faktum.svar())
     }
 
-    @Test fun `Dokument factory faktum`() {
+    @Test
+    fun `Dokument factory faktum`() {
         val søknadprosess = Søknad(95, dokument faktum "dokument" id 3).testSøknadprosess()
         val faktum = søknadprosess dokument 3
         assertFalse(faktum.erBesvart())
@@ -104,19 +99,5 @@ internal class BaseFaktumFactoryTest {
             assertTrue(faktum.erBesvart())
             assertEquals(it, faktum.svar())
         }
-    }
-
-    @Test fun `valg (enum) factory faktum`() {
-        val søknadprosess = Søknad(94, valg faktum "valg" id 3).testSøknadprosess()
-        val faktum = søknadprosess.id(3) as Faktum<Valg>
-        assertFalse(faktum.erBesvart())
-        assertThrows<IllegalStateException> { faktum.svar() }
-
-        faktum.besvar(A)
-        assertTrue(faktum.erBesvart())
-        assertEquals(A, faktum.svar())
-        faktum.besvar(C)
-        assertTrue(faktum.erBesvart())
-        assertEquals(C, faktum.svar())
     }
 }
