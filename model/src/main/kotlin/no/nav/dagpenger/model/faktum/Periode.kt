23f7588d5a4a52fd9cde376239d059045dd28b17
Geir André Lund <geir.andre.lund@nav.no>
2020-12-01 07:57:30 +0100

    Refaktorerte periode - fjernet overflødig metode

--- model/src/main/kotlin/no/nav/dagpenger/model/faktum/Periode.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/faktum/Periode.kt
@@ -4,23 +4,19 @@ import java.time.LocalDate
 
 class Periode(private val fom: LocalDate, private val tom: LocalDate) : ClosedRange<LocalDate>, Comparable<Periode> {
 
+    override val start: LocalDate = fom
+    override val endInclusive: LocalDate = tom
+
     init {
         require(fom < tom) { "Fra og med dato kan ikke være etter til og med dato" }
     }
-
-    internal infix fun inneholder(kandidat: LocalDate) = kandidat in fom..tom
 
     override fun equals(other: Any?): Boolean = other is Periode && other.fom == this.fom && other.tom == this.tom
     override operator fun compareTo(other: Periode): Int =
         other.start.compareTo(this.start) + other.endInclusive.compareTo(this.endInclusive)
 
-    override val endInclusive: LocalDate
-        get() = tom
-    override val start: LocalDate
-        get() = fom
-
-    override fun toString() = "$fom  til og med $tom"
+    override fun toString() = "$fom til og med $tom"
 }
 
-internal infix fun LocalDate.i(periode: Periode) = periode.inneholder(this)
+internal infix fun LocalDate.i(periode: Periode) = this in periode
 infix fun LocalDate.til(tom: LocalDate) = Periode(this, tom)
