6cf7a118243e3001b2db92bd237f4436c6513f44
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2020-12-07 10:35:08 +0100

    spotless
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/ApplicationBuilder.kt
@@ -2,10 +2,6 @@ package no.nav.dagpenger.quiz.mediator
 
 import PostgresDataSourceBuilder.clean
 import PostgresDataSourceBuilder.runMigration
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.dagpenger.quiz.mediator.db.SøknadRecord
 import no.nav.dagpenger.quiz.mediator.meldinger.DagensDatoService
 import no.nav.dagpenger.quiz.mediator.meldinger.FaktumSvarService
