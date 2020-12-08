5ca7f477ff0387a11b03dbdb212ad60e9ba0bfd5
André Roaldseth <andre.roaldseth@nav.no>
2020-12-08 13:35:02 +0100

    Legg på logg context

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/FaktumSvarService.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/FaktumSvarService.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.quiz.mediator.meldinger
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.dagpenger.model.faktum.Dokument
 import no.nav.dagpenger.model.faktum.Inntekt.Companion.årlig
 import no.nav.dagpenger.model.marshalling.ResultatJsonBuilder
@@ -41,43 +42,47 @@ internal class FaktumSvarService(
         val fakta = packet["fakta"].filter { faktumNode -> faktumNode.has("svar") }
         if (fakta.isEmpty()) return
         val søknadUuid = UUID.fromString(packet["søknad_uuid"].asText())
-        log.info { "Mottok ny svar for $søknadUuid" }
-        sikkerlogg.info { packet.toJson() }
 
-        try {
-            søknadPersistence.hent(søknadUuid, Versjon.UserInterfaceType.Web).also { søknadprosess ->
-                sikkerlogg.info {
-                    """Hentet lagrede fakta ${
-                    søknadprosess.søknad.filter { it.erBesvart() }.map { "${it.id}: ${it.svar()}" }
-                    } """
-                }
-                fakta.forEach { faktumNode ->
-                    val faktumId = faktumNode["id"].asText()
-                    val svar = faktumNode["svar"]
-                    val clazz = faktumNode["clazz"].asText()
+        withLoggingContext(
+            "id" to UUID.fromString(packet["id"].asText()).toString(),
+            "søknadUuid" to søknadUuid.toString()
+        ) {
+            log.info { "Mottok ny svar for $søknadUuid" }
+            sikkerlogg.info { packet.toJson() }
 
-                    besvar(søknadprosess, faktumId, svar, clazz)
-                }
-                søknadPersistence.lagre(søknadprosess.søknad)
-                søknadprosess.nesteSeksjoner()
-                    .onEach { seksjon ->
-                        val json = seksjon.somSpørsmål()
-                        context.send(json)
-                        sikkerlogg.info { "Send ut seksjon: $json" }
-                        log.info { "Send seksjon ${seksjon.navn} for søknad ${søknadprosess.søknad.uuid}" }
-                    }.also {
-                        if (Søknadprosess.erFerdig(it)) {
-                            ResultatJsonBuilder(søknadprosess).resultat().also { json ->
-                                context.send(json.toString())
-                                sikkerlogg.info { "Send ut resultat: $json" }
-                            }
-                            log.info { "Ferdig med søknad ${søknadprosess.søknad.uuid}. Resultatet er: ${søknadprosess.resultat()}" }
-                        }
+            try {
+                søknadPersistence.hent(søknadUuid, Versjon.UserInterfaceType.Web).also { søknadprosess ->
+                    sikkerlogg.info {
+                        """Hentet lagrede fakta ${
+                        søknadprosess.søknad.filter { it.erBesvart() }.map { "${it.id}: ${it.svar()}" }
+                        } """
                     }
-            }
-        } catch (e: Exception) {
-            log.error(e) {
-                "feil ved faktum svar: ${e.message}"
+                    fakta.forEach { faktumNode ->
+                        val faktumId = faktumNode["id"].asText()
+                        val svar = faktumNode["svar"]
+                        val clazz = faktumNode["clazz"].asText()
+
+                        besvar(søknadprosess, faktumId, svar, clazz)
+                    }
+                    søknadPersistence.lagre(søknadprosess.søknad)
+                    søknadprosess.nesteSeksjoner()
+                        .onEach { seksjon ->
+                            val json = seksjon.somSpørsmål()
+                            context.send(json)
+                            sikkerlogg.info { "Send ut seksjon: $json" }
+                            log.info { "Send seksjon ${seksjon.navn} for søknad ${søknadprosess.søknad.uuid}" }
+                        }.also {
+                            if (Søknadprosess.erFerdig(it)) {
+                                ResultatJsonBuilder(søknadprosess).resultat().also { json ->
+                                    context.send(json.toString())
+                                    sikkerlogg.info { "Send ut resultat: $json" }
+                                }
+                                log.info { "Ferdig med søknad ${søknadprosess.søknad.uuid}. Resultatet er: ${søknadprosess.resultat()}" }
+                            }
+                        }
+                }
+            } catch (e: Exception) {
+                log.error(e) { "feil ved svar for faktum: ${e.message}" }
             }
         }
     }
