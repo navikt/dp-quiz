35bcbed1d19920d6b4f4afe556d003347783ae02
Atle Nordland <atle.nordland@nav.no>
2021-01-26 09:08:48 +0100

    Bump soknad-versjon
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/AvslagPåMinsteinntektOppsett.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/AvslagPåMinsteinntektOppsett.kt
@@ -16,7 +16,7 @@ import no.nav.dagpenger.quiz.mediator.soknad.Seksjoner.søknadprosess
 // Forstår dagpengesøknaden
 internal object AvslagPåMinsteinntektOppsett {
     private val logger = KotlinLogging.logger { }
-    const val VERSJON_ID = 2
+    const val VERSJON_ID = 3
 
     fun registrer(registrer: (søknad: Søknad, versjonId: Int) -> Unit) {
         registrer(søknad, VERSJON_ID)
