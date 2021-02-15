29b13b7d7774f1293d12ee26f0ef59e4bd70f1bc
Geir André Lund <geir.andre.lund@nav.no>
2021-02-15 15:55:47 +0100

    Finne øvre og nedre terskelfaktor for "korona"-forskrifter av minstearbeidsinntekt
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/MinstearbeidsinntektTerskelStrategi.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/MinstearbeidsinntektTerskelStrategi.kt
@@ -1,25 +1,28 @@
 package no.nav.dagpenger.quiz.mediator.meldinger
 
-import java.lang.IllegalArgumentException
 import java.time.LocalDate
 
-object MinsteArbeidsinntektStrategi {
-    data class Terskel(
-        val nedre: Double,
-        val øvre: Double
+// Forstår terskelverdier for minste arbeidsinntekt
+object MinstearbeidsinntektFaktorStrategi {
+
+    private val terskler = listOf(
+        MinstearbeidsinntektTerskel(20.mars(2020), 30.oktober(2020), Faktor(0.75, 2.25)), // Forskrift
+        MinstearbeidsinntektTerskel(1.februar(2021), 30.juni(2021), Faktor(0.75, 2.25)), // Forskrift
+        MinstearbeidsinntektTerskel(LocalDate.MIN, LocalDate.MAX, Faktor(1.5, 3.0)) // https://lovdata.no/lov/1997-02-28-19/§4-4
     )
-    private val terskler = mutableListOf<MinsteArbeidsinntektStrategi.MinstearbeidsinntektTerskelStrategi>()
 
-    private val ordinær = MinstearbeidsinntektTerskelStrategi(LocalDate.MIN, LocalDate.MAX, Terskel(1.5, 3.0))
+    fun finnFaktor(virkningstidspunkt: LocalDate): Faktor {
+        return terskler.first { virkningstidspunkt in it }.faktor
+    }
 
-    private class MinstearbeidsinntektTerskelStrategi constructor(
+    private class MinstearbeidsinntektTerskel constructor(
         private val fom: LocalDate,
         private val tom: LocalDate,
-        val terskel: Terskel
+        val faktor: Faktor
     ) : ClosedRange<LocalDate> {
 
         init {
-            terskler.add(this)
+            require(fom.isBefore(tom)) { "Til og med '$fom' må være før Fra-og-med '$tom'" }
         }
 
         override val endInclusive: LocalDate
@@ -28,8 +31,21 @@ object MinsteArbeidsinntektStrategi {
             get() = fom
     }
 
-    fun finnTerskel(virkningstidspunkt: LocalDate): Terskel {
-        return terskler.find { virkningstidspunkt in it }?.terskel
-            ?: throw IllegalArgumentException("Fant ikke terskel basert på virkningstidspunkt $virkningstidspunkt")
-    }
+    data class Faktor(
+        val nedre: Double,
+        val øvre: Double
+    )
 }
+
+private fun Int.januar(year: Int) = LocalDate.of(year, 1, this)
+private fun Int.februar(year: Int) = LocalDate.of(year, 2, this)
+private fun Int.mars(year: Int) = LocalDate.of(year, 3, this)
+private fun Int.april(year: Int) = LocalDate.of(year, 4, this)
+private fun Int.mai(year: Int) = LocalDate.of(year, 5, this)
+private fun Int.juni(year: Int) = LocalDate.of(year, 6, this)
+private fun Int.juli(year: Int) = LocalDate.of(year, 7, this)
+private fun Int.august(year: Int) = LocalDate.of(year, 8, this)
+private fun Int.september(year: Int) = LocalDate.of(year, 9, this)
+private fun Int.oktober(year: Int) = LocalDate.of(year, 10, this)
+private fun Int.november(year: Int) = LocalDate.of(year, 11, this)
+private fun Int.desember(year: Int) = LocalDate.of(year, 12, this)
