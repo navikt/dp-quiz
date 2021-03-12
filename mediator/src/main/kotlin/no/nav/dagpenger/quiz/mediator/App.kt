f39020598629a0a611394cac3d2a3d4e9dd1d3b6
Kriise <knut.magne.riise@nav.no>
2021-03-12 14:16:30 +0100

    fjern sikkerlogg av konfig navikt/dagpenger#692
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/App.kt
@@ -1,10 +1,5 @@
 package no.nav.dagpenger.quiz.mediator
 
-import mu.KotlinLogging
-
-private val sikkerlogg = KotlinLogging.logger("tjenestekall")
-
 fun main() {
-    sikkerlogg.info { Configuration.config }
     ApplicationBuilder().start()
 }
