366da9118e414e26327ce08ffe0c346592d1c853
Kriise <knut.magne.riise@nav.no>
2021-03-12 13:37:23 +0100

    rapids and rivers config aiven navikt/dagpenger#692
    Co-authored-by: Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/Configuration.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/Configuration.kt
@@ -14,15 +14,18 @@ internal object Configuration {
 
     val rapidApplication: Map<String, String> = mutableMapOf(
         "RAPID_APP_NAME" to "dp-quiz-mediator",
-        "KAFKA_BOOTSTRAP_SERVERS" to config.getOrElse(Key("kafka.bootstrap.servers", stringType), "localhost:9092"),
+        "KAFKA_BROKERS" to config.getOrElse(Key("kafka.brokers", stringType), "localhost:9092"),
         "KAFKA_CONSUMER_GROUP_ID" to "dp-quiz-mediator-v1",
-        "KAFKA_RAPID_TOPIC" to config.getOrElse(Key("kafka.topic", stringType), "privat-dagpenger-behov-v2"),
+        "KAFKA_RAPID_TOPIC" to config.getOrElse(Key("kafka.topic", stringType), "teamdagpenger.rapid.v1"),
         "KAFKA_RESET_POLICY" to config.getOrElse(Key("kafka.reset.policy", stringType), "latest"),
-        "NAV_TRUSTSTORE_PASSWORD" to config.getOrElse(Key("nav.truststore.password", stringType), "/non/existing"),
+        "KAFKA_CREDSTORE_PASSWORD" to config.getOrElse(Key("kafka.credstore.password", stringType), "/non/existing"),
         "HTTP_PORT" to config.getOrElse(Key("port", stringType), "8080")
     ).also {
-        config.getOrNull(Key("nav.truststore.path", stringType))?.let { truststore ->
-            it["NAV_TRUSTSTORE_PATH"] = truststore
+        config.getOrNull(Key("kafka.truststore.path", stringType))?.let { truststore ->
+            it["KAFKA_TRUSTSTORE_PATH"] = truststore
+        }
+        config.getOrNull(Key("kafka.keystore.path", stringType))?.let { keystore ->
+            it["KAFKA_KEYSTORE_PATH"] = keystore
         }
     }
 }
