40b070ef5f5d673cf1b23ea91e69995e0f9fb128
Atle Nordland <atle.nordland@nav.no>
2021-07-21 09:30:39 +0200

    Lagre grunn til manuell behandling
    ref navikt/dagpenger#885
    
    Co-authored-by: marte pedersen <marte.pedersen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/ManuellBehandlingSink.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/ManuellBehandlingSink.kt
@@ -1,28 +1,43 @@
 package no.nav.dagpenger.quiz.mediator.meldinger
 
 import mu.KotlinLogging
+import no.nav.dagpenger.quiz.mediator.db.ResultatPersistence
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import java.util.UUID
 
 class ManuellBehandlingSink(
-    rapidsConnection: RapidsConnection
+    rapidsConnection: RapidsConnection,
+    private val resultatPersistence: ResultatPersistence
 ) : River.PacketListener {
 
     companion object {
         private val log = KotlinLogging.logger {}
+        private val sikkerlogg = KotlinLogging.logger("tjenestekall")
     }
 
     init {
         River(rapidsConnection).apply {
             validate {
                 it.demandValue("@event_name", "manuell_behandling")
+                it.requireKey("søknad_uuid", "seksjon_navn")
             }
         }.register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         log.info { "Mottok melding om manuell behandling" }
+        val søknadUuid = UUID.fromString(packet["søknad_uuid"].asText())
+        val seksjonNavn = packet["seksjon_navn"].asText()
+
+        resultatPersistence.lagreManuellBehandling(søknadUuid, seksjonNavn)
+    }
+
+    override fun onError(problems: MessageProblems, context: MessageContext) {
+        log.error { problems.toString() }
+        sikkerlogg.error { problems.toExtendedReport() }
     }
 }
--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/ManuellBehandlingSinkTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/meldinger/ManuellBehandlingSinkTest.kt
@@ -1,7 +1,11 @@
 package no.nav.dagpenger.quiz.mediator.meldinger
 
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.dagpenger.quiz.mediator.db.ResultatPersistence
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.Test
+import java.util.UUID
 
 internal class ManuellBehandlingSinkTest {
 
@@ -9,14 +13,24 @@ internal class ManuellBehandlingSinkTest {
 
     @Test
     fun `kræsjer ikke når man kjører den opp, og logger`() {
-        ManuellBehandlingSink(testRapid)
+        val resultatPersistence = mockk<ResultatPersistence>(relaxed = true)
 
-        //language=JSON
+        ManuellBehandlingSink(testRapid, resultatPersistence)
+
+        val uuid = UUID.randomUUID()
+        val manuellSeksjonNavn = "manuell"
+
         val melding =
             """
-            { "@event_name": "manuell_behandling" }
+            { 
+              "@event_name": "manuell_behandling",
+              "søknad_uuid": "$uuid",
+              "seksjon_navn": "$manuellSeksjonNavn"
+            }
             """.trimIndent()
 
         testRapid.sendTestMessage(melding)
+
+        verify(exactly = 1) { resultatPersistence.lagreManuellBehandling(uuid, manuellSeksjonNavn) }
     }
 }
