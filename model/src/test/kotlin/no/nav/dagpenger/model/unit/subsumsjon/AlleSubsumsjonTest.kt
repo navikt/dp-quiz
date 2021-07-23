eb92fa07d0c6860dad00ed5063d994739ebf214f
marte pedersen <marte.pedersen@nav.no>
2021-07-23 14:47:42 +0200

    forsøk på litt mer beskrivende tester for de sammensatte subsumsjonene

--- model/src/test/kotlin/no/nav/dagpenger/model/unit/subsumsjon/AlleSubsumsjonTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/unit/subsumsjon/AlleSubsumsjonTest.kt
@@ -5,8 +5,8 @@ import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.helpers.testSøknadprosess
 import no.nav.dagpenger.model.regel.er
 import no.nav.dagpenger.model.subsumsjon.alle
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 internal class AlleSubsumsjonTest {
@@ -21,20 +21,28 @@ internal class AlleSubsumsjonTest {
     private val neida = søknad boolsk 1
     private val joda = søknad boolsk 2
     private val ja1 = søknad boolsk 3
-    private val alle = "både joda eller neida".alle(
+    private val alle = "både joda og neida".alle(
         neida er true,
         joda er false,
         ja1 er true
     )
 
     @Test
-    fun `skal være true bare hvis alle faktumene i subsumsjonen er lik regelene`() {
+    fun `skal være true bare hvis alle undersubsumsjonene er true`() {
         val søknadsprosess = søknad.testSøknadprosess(alle)
         søknadsprosess.boolsk(1).besvar(true)
         søknadsprosess.boolsk(2).besvar(false)
         søknadsprosess.boolsk(3).besvar(true)
-        assertTrue(søknadsprosess.resultat()!!)
+        assertEquals(true, søknadsprosess.resultat())
+
         søknadsprosess.boolsk(2).besvar(true)
-        assertFalse(søknadsprosess.resultat()!!)
+        assertEquals(false, søknadsprosess.resultat())
+    }
+
+    @Test
+    fun `alle sjekker alle undersubsumsjoner, resultatet er ikke klart før alle er besvart`() {
+        val søknadsprosess = søknad.testSøknadprosess(alle)
+        søknadsprosess.boolsk(1).besvar(false)
+        assertNull(søknadsprosess.resultat())
     }
 }
--- model/src/test/kotlin/no/nav/dagpenger/model/unit/subsumsjon/MinstEnAvSubsumsjonTest.kt
+++ model/src/test/kotlin/no/nav/dagpenger/model/unit/subsumsjon/MinstEnAvSubsumsjonTest.kt
@@ -1,48 +1,48 @@
 package no.nav.dagpenger.model.unit.subsumsjon
 
-import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.boolsk
+import no.nav.dagpenger.model.factory.BaseFaktumFactory
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.helpers.testSøknadprosess
 import no.nav.dagpenger.model.regel.er
-import no.nav.dagpenger.model.subsumsjon.bareEnAv
+import no.nav.dagpenger.model.subsumsjon.minstEnAv
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
-class BareEnAvSubsumsjonTest {
+internal class MinstEnAvSubsumsjonTest {
 
     private val søknad = Søknad(
         0,
-        boolsk faktum "neida" id 1,
-        boolsk faktum "joda" id 2,
-        boolsk faktum "ja" id 3
+        BaseFaktumFactory.Companion.boolsk faktum "neida" id 1,
+        BaseFaktumFactory.Companion.boolsk faktum "joda" id 2,
+        BaseFaktumFactory.Companion.boolsk faktum "ja" id 3
     )
 
     private val neida = søknad boolsk 1
     private val joda = søknad boolsk 2
     private val ja1 = søknad boolsk 3
-    private val bareEnAv = "Enten joda eller neida".bareEnAv(
+    private val minstEnAv = "Enten joda eller neida".minstEnAv(
         neida er true,
         joda er true,
         ja1 er true
     )
 
     @Test
-    fun `skal være true bare hvis en av undersubsumsjonene er true`() {
+    fun `skal være true hvis resultatet av minst én undersubsumsjon er true`() {
 
-        val søknadsprosess = søknad.testSøknadprosess(bareEnAv)
-        søknadsprosess.boolsk(1).besvar(true)
-        søknadsprosess.boolsk(2).besvar(false)
+        val søknadsprosess = søknad.testSøknadprosess(minstEnAv)
+        søknadsprosess.boolsk(1).besvar(false)
+        søknadsprosess.boolsk(2).besvar(true)
         søknadsprosess.boolsk(3).besvar(false)
         assertEquals(true, søknadsprosess.resultat())
 
-        søknadsprosess.boolsk(2).besvar(true)
+        søknadsprosess.boolsk(2).besvar(false)
         assertEquals(false, søknadsprosess.resultat())
     }
 
     @Test
-    fun `bareEnAv sjekker alle undersubsumsjoner, resultatet er ikke klart før alle er besvart`() {
-        val søknadsprosess = søknad.testSøknadprosess(bareEnAv)
+    fun `minstEnAv sjekker alle undersubsumsjoner, resultatet er ikke klart før alle er besvart`() {
+        val søknadsprosess = søknad.testSøknadprosess(minstEnAv)
         søknadsprosess.boolsk(1).besvar(false)
         assertNull(søknadsprosess.resultat())
     }
