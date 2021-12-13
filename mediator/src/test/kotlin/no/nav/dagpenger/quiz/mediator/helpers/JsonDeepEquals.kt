aab3b4d57041848076d786279cc4c1c9e06d2a5a
Geir André Lund <geir.andre.lund@nav.no>
2021-12-13 11:46:06 +0100

    Litt mer jobb med deepequals

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/JsonDeepEquals.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/JsonDeepEquals.kt
@@ -1,7 +1,6 @@
 package no.nav.dagpenger.quiz.mediator.helpers
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.PropertyAccessor
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
@@ -10,35 +9,13 @@ import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import no.nav.dagpenger.model.factory.FaktaRegel
 import no.nav.dagpenger.model.faktum.Faktum
-import no.nav.dagpenger.model.faktum.GeneratorFaktum
 import no.nav.dagpenger.model.faktum.GrunnleggendeFaktum
 import no.nav.dagpenger.model.faktum.Rolle
-import no.nav.dagpenger.model.faktum.TemplateFaktum
 import no.nav.dagpenger.model.faktum.Valg
 import no.nav.dagpenger.model.faktum.ValgFaktum
 import no.nav.dagpenger.model.visitor.FaktumVisitor
 import org.junit.jupiter.api.Assertions.assertEquals
-
-@JsonIgnoreProperties("avhengigeFakta", "avhengerAvFakta")
-private class GrunnlegendeFaktumMixin
-
-@JsonIgnoreProperties("seksjoner")
-private class TemplateFaktumMixin
-
-@JsonIgnoreProperties("søknad")
-private class GeneratorFaktumMixin
-
-private class FaktaRegelSerializer : JsonSerializer<FaktaRegel<*>?>() {
-    override fun serialize(
-        regel: FaktaRegel<*>?,
-        jsonGenerator: JsonGenerator,
-        serializers: SerializerProvider?
-    ) {
-        regel?.let { jsonGenerator.writeString(it.navn) }
-    }
-}
 
 private class FaktumSerializer : JsonSerializer<Faktum<*>>() {
     override fun serialize(
@@ -164,12 +141,6 @@ private class FaktumVisitor(
 private val module = SimpleModule()
     .also {
         it.addSerializer(
-            FaktaRegel::class.java,
-            FaktaRegelSerializer()
-        )
-    }
-    .also {
-        it.addSerializer(
             Faktum::class.java,
             FaktumSerializer()
         )
@@ -179,13 +150,6 @@ private val objectMapper = jacksonObjectMapper()
     .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
     .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     .registerModule(module)
-    .setMixIns(
-        mutableMapOf(
-            GrunnleggendeFaktum::class.java to GrunnlegendeFaktumMixin::class.java,
-            TemplateFaktum::class.java to TemplateFaktumMixin::class.java,
-            GeneratorFaktum::class.java to GeneratorFaktumMixin::class.java,
-        ) as Map<Class<*>, Class<*>>
-    )
     .setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
     .registerModule(JavaTimeModule())
 
