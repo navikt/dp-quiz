7d6f009515318e4f9ba05f3a7d4c85502095b7cf
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2022-02-03 10:12:09 +0100

    refactor: Forbedret feilhåndtering, og sørger for å skippe aller filer som ikke trengs.
    DAG-116
    
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/genererDsl.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/genererDsl.kt
@@ -3,27 +3,35 @@ package no.nav.dagpenger.quiz.mediator.helpers
 import java.io.File
 
 private val homeDirectory = System.getProperty("user.home")
-private val quizshowPath = "$homeDirectory/pathTilQuizshow"
+private val quizshowPath = "$homeDirectory/path-til-dp-soknad"
 private val faktaPath = "$quizshowPath/src/soknad-fakta"
 
 fun main() {
-    val komplettFakta = File(faktaPath)
-    if (verifiserAtFilEksisterer(komplettFakta)) return
-    val faktaFiles: Array<File>? = komplettFakta.listFiles { file -> !file.name.contains("soknad.ts") }
+    val faktaseksjoner = lesInnFaktaseksjoner()
 
-    faktaFiles?.forEach { file ->
-        println("Fil: $file")
-        val fileAsString = file.readText(Charsets.UTF_8)
+    faktaseksjoner?.forEach { seksjonsfil ->
+        println("Fil: $seksjonsfil")
+        val fileAsString = seksjonsfil.readText(Charsets.UTF_8)
         val fileAsJson = fileAsString.fjernTypescriptSyntax()
         val quizDsl = BffTilDslGenerator(fileAsJson)
         println("$quizDsl\n")
     }
 }
 
-private fun verifiserAtFilEksisterer(komplettFakta: File): Boolean {
-    if (!komplettFakta.exists()) {
-        println("Fant ikke mappa, lette etter $komplettFakta")
-        return true
+private fun lesInnFaktaseksjoner(): Array<File>? {
+    val mappeMedFrontendFaktaseksjoner = File(faktaPath)
+    verifiserAtMappaEksisterer(mappeMedFrontendFaktaseksjoner)
+    val faktaseksjoner: Array<File>? = mappeMedFrontendFaktaseksjoner.listFiles { seksjonsfil ->
+        filterBortUnødvendigeSeksjoner(seksjonsfil)
     }
-    return false
+    return faktaseksjoner
 }
+
+private fun verifiserAtMappaEksisterer(path: File) {
+    if (!path.exists()) {
+        throw IllegalArgumentException("Fant ikke mappa: '$path'. Vennligst spesifiser lokal path til 'dp-soknad'.")
+    }
+}
+
+private fun filterBortUnødvendigeSeksjoner(file: File) =
+    !(file.name.contains("soknad.ts") || file.name.contains("dummy-seksjon.ts"))
