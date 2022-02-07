c78b9e9669a4ba01ecc399ce202519ccab7c19fe
Celine Dam <celine.ten.dam@nav.no>
2022-02-07 12:23:39 +0100

    refactor: Endre variabelnavn på path fra "quizshow" til "soknadsdialog"

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/genererDsl.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/genererDsl.kt
@@ -3,8 +3,8 @@ package no.nav.dagpenger.quiz.mediator.helpers
 import java.io.File
 
 private val homeDirectory = System.getProperty("user.home")
-private val quizshowPath = "$homeDirectory/IdeaProjects/dp-soknadsdialog"
-private val faktaPath = "$quizshowPath/src/soknad-fakta"
+private val soknadsdialogPath = "$homeDirectory/pathTo/dp-soknadsdialog"
+private val faktaPath = "$soknadsdialogPath/src/soknad-fakta"
 
 fun main() {
     val faktaseksjoner = lesInnFaktaseksjoner()
