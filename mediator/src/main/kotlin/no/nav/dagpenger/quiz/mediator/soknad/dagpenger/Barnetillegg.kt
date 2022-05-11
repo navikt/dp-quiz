fcfa96f71a1df4993fa00bf591316e2a7131a977
Celine ten Dam <celine.ten.dam@nav.no>
2022-05-11 10:54:11 +0200

    WIP: Startet på regeltre for barnetillegg.
    DAG-254

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Barnetillegg.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Barnetillegg.kt
@@ -8,7 +8,14 @@ import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.tekst
 import no.nav.dagpenger.model.faktum.Rolle
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.faktum.Søknad.Companion.seksjon
+import no.nav.dagpenger.model.regel.er
+import no.nav.dagpenger.model.regel.har
+import no.nav.dagpenger.model.regel.utfylt
 import no.nav.dagpenger.model.seksjon.Seksjon
+import no.nav.dagpenger.model.subsumsjon.Subsumsjon
+import no.nav.dagpenger.model.subsumsjon.alle
+import no.nav.dagpenger.model.subsumsjon.deltre
+import no.nav.dagpenger.model.subsumsjon.hvisOppfylt
 import no.nav.dagpenger.quiz.mediator.soknad.DslFaktaseksjon
 
 object Barnetillegg : DslFaktaseksjon {
@@ -47,4 +54,34 @@ object Barnetillegg : DslFaktaseksjon {
 
         return listOf(barnetilleggRegister, barnetilleggSøker)
     }
+
+    fun regeltre(søknad: Søknad): Subsumsjon = with(søknad) {
+        "§ x-x Barnetillegg".alle(
+            generator(`barn liste`) har "et eller flere barn".deltre {
+                `info om barnet`().hvisOppfylt {
+                    `forsørger barnet`()
+                }.hvisOppfylt {
+                    `har barnet årsinntekt på over 1 år`().hvisOppfylt {
+                        `barnets inntekt`()
+                    }
+                }
+            }
+        )
+    }
+
+    private fun Søknad.`info om barnet`() = "info".alle(
+        tekst(`barn fornavn mellomnavn`).utfylt(),
+        tekst(`barn etternavn`).utfylt(),
+        dato(`barn foedselsdato`).utfylt(),
+        land(`barn bostedsland`).utfylt(),
+    )
+
+    private fun Søknad.`forsørger barnet`() =
+        boolsk(`forsoerger du barnet`) er true
+
+    private fun Søknad.`har barnet årsinntekt på over 1 år`() =
+        boolsk(`barn aarsinntekt over 1g`) er true
+
+    private fun Søknad.`barnets inntekt`() =
+        heltall(`barn inntekt`).utfylt()
 }
--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/BarnetilleggTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/BarnetilleggTest.kt
@@ -1,12 +1,50 @@
 package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
 
+import no.nav.dagpenger.model.faktum.Land
+import no.nav.dagpenger.model.faktum.Prosessversjon
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.faktum.Tekst
+import no.nav.dagpenger.quiz.mediator.helpers.testSøknadprosess
+import no.nav.dagpenger.quiz.mediator.soknad.Prosess
 import no.nav.dagpenger.quiz.mediator.soknad.verifiserFeltsammensetting
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
 
 internal class BarnetilleggTest {
 
     @Test
     fun `Sjekk om faktasammensettingen har endret seg siden sist`() {
         Barnetillegg.verifiserFeltsammensetting(8, 8036)
+    }
+
+    @Test
+    fun `Regeltre barnetillegg`(){
+        val søknad = Søknad(Prosessversjon(Prosess.Dagpenger, -1), *Barnetillegg.fakta())
+        val søknadprosess = søknad.testSøknadprosess(
+            Barnetillegg.regeltre(søknad)
+        )
+
+        søknadprosess.generator(Barnetillegg.`barn liste`).besvar(0)
+        assertEquals(false, søknadprosess.resultat()) //Denne skal egentlig bli true
+
+        søknadprosess.generator(Barnetillegg.`barn liste`).besvar(1)
+        assertEquals(null, søknadprosess.resultat())
+
+
+        søknadprosess.tekst(Barnetillegg.`barn fornavn mellomnavn`).besvar(Tekst("Ola"))
+        søknadprosess.tekst(Barnetillegg.`barn etternavn`).besvar(Tekst("Nordmann"))
+        søknadprosess.dato(Barnetillegg.`barn foedselsdato`).besvar(LocalDate.now().minusYears(1))
+        søknadprosess.land(Barnetillegg.`barn bostedsland`).besvar(Land("NOR"))
+        søknadprosess.boolsk(Barnetillegg.`forsoerger du barnet`).besvar(true)
+        //assertEquals(true, søknadprosess.resultat())   Denne skal være true dersom spm over besvares med false
+
+        søknadprosess.boolsk(Barnetillegg.`forsoerger du barnet`).besvar(true)
+        søknadprosess.boolsk(Barnetillegg.`barn aarsinntekt over 1g`).besvar(true)
+        søknadprosess.heltall(Barnetillegg.`barn inntekt`).besvar(100)
+        assertEquals(true, søknadprosess.resultat()) //Hvorfor blir denne null
+
     }
 }
