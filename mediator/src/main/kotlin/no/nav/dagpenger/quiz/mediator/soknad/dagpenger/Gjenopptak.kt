9495f24bae1564779be3af74c00c1d6ab7d5c821
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2022-05-11 14:59:06 +0200

    Har lagt til egen bolk og seksjon for spørsmålet om bruker har mottatt dagpenger det siste året.
    Det er dette faktumet som skal brukes ifm gjennopptak.
    
    DAG-259

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Gjenopptak.kt
@@ -0,0 +1,26 @@
+package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
+
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.envalg
+import no.nav.dagpenger.model.faktum.Rolle
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.faktum.Søknad.Companion.seksjon
+import no.nav.dagpenger.model.regel.utfylt
+import no.nav.dagpenger.model.subsumsjon.Subsumsjon
+import no.nav.dagpenger.quiz.mediator.soknad.DslFaktaseksjon
+
+object Gjenopptak : DslFaktaseksjon {
+    const val `mottatt dagpenger siste 12 mnd` = 10001
+
+    override val fakta = listOf(
+        envalg faktum "faktum.mottatt-dagpenger-siste-12-mnd"
+            med "svar.ja"
+            med "svar.nei"
+            med "svar.vet-ikke" id `mottatt dagpenger siste 12 mnd`
+    )
+
+    override fun seksjon(søknad: Søknad) = listOf(søknad.seksjon("gjenopptak", Rolle.søker, *this.databaseIder()))
+
+    fun regeltre(søknad: Søknad): Subsumsjon = with(søknad) {
+        boolsk(`mottatt dagpenger siste 12 mnd`).utfylt()
+    }
+}
---
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/GjenopptakTest.kt
@@ -0,0 +1,36 @@
+package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
+
+import no.nav.dagpenger.model.faktum.Envalg
+import no.nav.dagpenger.model.faktum.Prosessversjon
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.quiz.mediator.helpers.testSøknadprosess
+import no.nav.dagpenger.quiz.mediator.soknad.Prosess
+import no.nav.dagpenger.quiz.mediator.soknad.verifiserFeltsammensetting
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class GjenopptakTest {
+
+    @Test
+    fun `Sjekk om faktasammensettingen har endret seg siden sist`() {
+        Gjenopptak.verifiserFeltsammensetting(1, 10001)
+    }
+
+    @Test
+    fun `Det må svares på om man har mottat dagpenger det siste året`() {
+        val søknad = Søknad(Prosessversjon(Prosess.Dagpenger, -1), *Gjenopptak.fakta())
+        val søknadprosess = søknad.testSøknadprosess(
+            Gjenopptak.regeltre(søknad)
+        )
+
+        assertEquals(null, søknadprosess.resultat())
+        søknadprosess.envalg(Gjenopptak.`mottatt dagpenger siste 12 mnd`).besvar(Envalg("faktum.mottatt-dagpenger-siste-12-mnd.svar.ja"))
+        assertEquals(true, søknadprosess.resultat())
+
+        søknadprosess.envalg(Gjenopptak.`mottatt dagpenger siste 12 mnd`).besvar(Envalg("faktum.mottatt-dagpenger-siste-12-mnd.svar.nei"))
+        assertEquals(true, søknadprosess.resultat())
+
+        søknadprosess.envalg(Gjenopptak.`mottatt dagpenger siste 12 mnd`).besvar(Envalg("faktum.mottatt-dagpenger-siste-12-mnd.svar.vet-ikke"))
+        assertEquals(true, søknadprosess.resultat())
+    }
+}
