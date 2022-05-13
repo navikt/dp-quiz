4a8bfeb6a41428350fdaf9b32335b3f811c41e9b
Marius Eriksen <marius.eriksen@nav.no>
2022-05-13 10:00:30 +0200

    wip: Start på regeltre for reell arbeidssøker. DAG-249

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/ReellArbeidssoker.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/ReellArbeidssoker.kt
@@ -7,12 +7,17 @@ import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.tekst
 import no.nav.dagpenger.model.faktum.Rolle
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.faktum.Søknad.Companion.seksjon
+import no.nav.dagpenger.model.regel.er
+import no.nav.dagpenger.model.regel.utfylt
+import no.nav.dagpenger.model.subsumsjon.Subsumsjon
+import no.nav.dagpenger.model.subsumsjon.hvisOppfylt
+import no.nav.dagpenger.model.subsumsjon.minstEnAv
 import no.nav.dagpenger.quiz.mediator.soknad.DslFaktaseksjon
 
 object ReellArbeidssoker : DslFaktaseksjon {
 
-    const val `jobbe hel deltid` = 1
-    const val `kun deltid aarsak` = 2
+    const val `Kan jobbe hel og deltid` = 1
+    const val `Årsak til kun deltid` = 2
     const val `kort om hvorfor kun deltid` = 3
     const val `kun deltid aarsak antall timer` = 4
     const val `jobbe hele norge` = 5
@@ -21,8 +26,9 @@ object ReellArbeidssoker : DslFaktaseksjon {
     const val `alle typer arbeid` = 8
     const val `bytte yrke ned i lonn` = 9
 
+    // TODO: Konstanter med flervalgsvar?
     override val fakta = listOf(
-        boolsk faktum "faktum.jobbe-hel-deltid" id `jobbe hel deltid`,
+        boolsk faktum "faktum.jobbe-hel-deltid" id `Kan jobbe hel og deltid`,
         flervalg faktum "faktum.kun-deltid-aarsak"
             med "svar.redusert-helse"
             med "svar.omsorg-baby"
@@ -30,7 +36,7 @@ object ReellArbeidssoker : DslFaktaseksjon {
             med "svar.omsorg-barn-spesielle-behov"
             med "svar.skift-turnus"
             med "svar.har-fylt-60"
-            med "svar.annen-situasjon" id `kun deltid aarsak`,
+            med "svar.annen-situasjon" id `Årsak til kun deltid`,
         tekst faktum "faktum.kort-om-hvorfor-kun-deltid" id `kort om hvorfor kun deltid`,
         heltall faktum "faktum.kun-deltid-aarsak-antall-timer" id `kun deltid aarsak antall timer`,
         boolsk faktum "faktum.jobbe-hele-norge" id `jobbe hele norge`,
@@ -46,6 +52,15 @@ object ReellArbeidssoker : DslFaktaseksjon {
         boolsk faktum "faktum.alle-typer-arbeid" id `alle typer arbeid`,
         boolsk faktum "faktum.bytte-yrke-ned-i-lonn" id `bytte yrke ned i lonn`
     )
+
+    fun regeltre(søknad: Søknad): Subsumsjon = with(søknad) {
+        "Er reel arbeidssøker".minstEnAv(
+            boolsk(`Kan jobbe hel og deltid`) er true,
+            boolsk(`Kan jobbe hel og deltid`) er false hvisOppfylt {
+                flervalg(`Årsak til kun deltid`).utfylt()
+            }
+        )
+    }
 
     override fun seksjon(søknad: Søknad) = listOf(søknad.seksjon("reell-arbeidssoker", Rolle.søker, *this.databaseIder()))
 }
--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/ReellArbeidssokerTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/ReellArbeidssokerTest.kt
@@ -1,6 +1,12 @@
 package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
 
+import no.nav.dagpenger.model.faktum.Flervalg
+import no.nav.dagpenger.model.faktum.Prosessversjon
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.quiz.mediator.helpers.testSøknadprosess
+import no.nav.dagpenger.quiz.mediator.soknad.Prosess
 import no.nav.dagpenger.quiz.mediator.soknad.verifiserFeltsammensetting
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class ReellArbeidssokerTest {
@@ -8,5 +14,22 @@ internal class ReellArbeidssokerTest {
     @Test
     fun `Sjekk om faktasammensettingen har endret seg siden sist`() {
         ReellArbeidssoker.verifiserFeltsammensetting(9, 45)
+    }
+
+    @Test
+    fun `regeltre for reel arbeidssøker`() {
+        val søknad = Søknad(Prosessversjon(Prosess.Dagpenger, -1), *ReellArbeidssoker.fakta())
+        val søknadprosess = søknad.testSøknadprosess(
+            ReellArbeidssoker.regeltre(søknad)
+        )
+
+        assertEquals(null, søknadprosess.resultat())
+
+        søknadprosess.boolsk(ReellArbeidssoker.`Kan jobbe hel og deltid`).besvar(false)
+        assertEquals(null, søknadprosess.resultat())
+        søknadprosess.boolsk(ReellArbeidssoker.`Kan jobbe hel og deltid`).besvar(true)
+        assertEquals(true, søknadprosess.resultat())
+        søknadprosess.flervalg(ReellArbeidssoker.`Årsak til kun deltid`).besvar(Flervalg("faktum.kun-deltid-aarsak.svar.annen-situasjon"))
+        assertEquals(true, søknadprosess.resultat())
     }
 }
