c603c891c03585fc7b1fcdcc42faa66ee4ab7d51
Celine ten Dam <celine.ten.dam@nav.no>
2022-05-19 12:50:10 +0200

    Revert "Bumpe versjon"
    This reverts commit e88c93967d87b8ee4010f378279c7beb585cde7c.

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Dagpenger.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Dagpenger.kt
@@ -15,7 +15,7 @@ internal object Dagpenger {
 
     private val logger = KotlinLogging.logger { }
 
-    val VERSJON_ID = Prosessversjon(Prosess.Dagpenger, 216)
+    val VERSJON_ID = Prosessversjon(Prosess.Dagpenger, 215)
 
     fun registrer(registrer: (søknad: Søknad) -> Unit) {
         registrer(søknad)
