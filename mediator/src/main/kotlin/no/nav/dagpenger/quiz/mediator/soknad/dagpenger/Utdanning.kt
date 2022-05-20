58d0eeddcda0599e7b9d5138e04404bc5a946225
Celine ten Dam <celine.ten.dam@nav.no>
2022-05-20 08:01:15 +0200

    Regeltre for utdanning.
    DAG-247

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Utdanning.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Utdanning.kt
@@ -4,6 +4,11 @@ import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.boolsk
 import no.nav.dagpenger.model.faktum.Rolle
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.faktum.Søknad.Companion.seksjon
+import no.nav.dagpenger.model.regel.er
+import no.nav.dagpenger.model.regel.utfylt
+import no.nav.dagpenger.model.subsumsjon.alle
+import no.nav.dagpenger.model.subsumsjon.hvisOppfylt
+import no.nav.dagpenger.model.subsumsjon.minstEnAv
 import no.nav.dagpenger.quiz.mediator.soknad.DslFaktaseksjon
 
 object Utdanning : DslFaktaseksjon {
@@ -14,9 +19,23 @@ object Utdanning : DslFaktaseksjon {
 
     override val fakta = listOf(
         boolsk faktum "faktum.tar-du-utdanning" id `tar du utdanning`,
-        boolsk faktum "faktum.avsluttet-utdanning-siste-6-mnd" id `avsluttet utdanning siste 6 mnd`,
-        boolsk faktum "faktum.planlegger-utdanning-med-dagpenger" id `planlegger utdanning med dagpenger`
+        boolsk faktum "faktum.avsluttet-utdanning-siste-6-mnd" id `avsluttet utdanning siste 6 mnd` avhengerAv `tar du utdanning`,
+        boolsk faktum "faktum.planlegger-utdanning-med-dagpenger" id `planlegger utdanning med dagpenger` avhengerAv `tar du utdanning`
     )
 
     override fun seksjon(søknad: Søknad) = listOf(søknad.seksjon("utdanning", Rolle.søker, *this.databaseIder()))
+
+    fun regeltre(søknad: Søknad) = with(søknad) {
+        "Utdanning".alle(
+            "tar utdanning eller ikke".minstEnAv(
+                boolsk(`tar du utdanning`) er true,
+                boolsk(`tar du utdanning`) er false hvisOppfylt {
+                    "nylig avsluttet utdanning eller planer om utdanning".alle(
+                        boolsk(`avsluttet utdanning siste 6 mnd`).utfylt(),
+                        boolsk(`planlegger utdanning med dagpenger`).utfylt()
+                    )
+                }
+            )
+        )
+    }
 }
--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/UtdanningTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/UtdanningTest.kt
@@ -1,12 +1,74 @@
 package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
 
+import no.nav.dagpenger.model.faktum.Faktum
+import no.nav.dagpenger.model.faktum.Prosessversjon
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.seksjon.Søknadprosess
+import no.nav.dagpenger.quiz.mediator.helpers.testSøknadprosess
+import no.nav.dagpenger.quiz.mediator.soknad.Prosess
 import no.nav.dagpenger.quiz.mediator.soknad.verifiserFeltsammensetting
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 internal class UtdanningTest {
+
+    private val søknad = Søknad(Prosessversjon(Prosess.Dagpenger, -1), *Utdanning.fakta())
+    private lateinit var søknadprosess: Søknadprosess
+    private lateinit var tarUtdanning: Faktum<Boolean>
+    private lateinit var nyligAvsluttetUtdanning: Faktum<Boolean>
+    private lateinit var planleggerUtdanning: Faktum<Boolean>
 
     @Test
     fun `Sjekk om faktasammensettingen har endret seg siden sist`() {
         Utdanning.verifiserFeltsammensetting(3, 6006)
     }
+
+    @BeforeEach
+    fun setup() {
+        søknadprosess = søknad.testSøknadprosess(Utdanning.regeltre(søknad))
+
+        tarUtdanning = søknadprosess.boolsk(Utdanning.`tar du utdanning`)
+        nyligAvsluttetUtdanning = søknadprosess.boolsk(Utdanning.`avsluttet utdanning siste 6 mnd`)
+        planleggerUtdanning = søknadprosess.boolsk(Utdanning.`planlegger utdanning med dagpenger`)
+    }
+
+    @Test
+    fun `Tar utdanning`() {
+        tarUtdanning.besvar(true)
+        assertEquals(true, søknadprosess.resultat())
+    }
+
+    @Test
+    fun `Ingen utdanning`() {
+        tarUtdanning.besvar(false)
+        assertEquals(null, søknadprosess.resultat())
+
+        nyligAvsluttetUtdanning.besvar(true)
+        planleggerUtdanning.besvar(true)
+        assertEquals(true, søknadprosess.resultat())
+    }
+
+    @Test
+    fun avhengigheter() {
+        tarUtdanning.besvar(false)
+        nyligAvsluttetUtdanning.besvar(true)
+        planleggerUtdanning.besvar(true)
+        erBesvart(nyligAvsluttetUtdanning, planleggerUtdanning)
+
+        tarUtdanning.besvar(true)
+        erUbesvart(nyligAvsluttetUtdanning, planleggerUtdanning)
+    }
+
+    private fun erBesvart(vararg fakta: Faktum<*>) =
+        fakta.forEach { faktum ->
+            assertTrue(faktum.erBesvart())
+        }
+
+    private fun erUbesvart(vararg fakta: Faktum<*>) =
+        fakta.forEach { faktum ->
+            assertFalse(faktum.erBesvart())
+        }
 }
