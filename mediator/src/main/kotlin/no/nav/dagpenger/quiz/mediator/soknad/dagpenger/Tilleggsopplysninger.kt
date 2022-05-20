aa7d2d7c19e3b466a13b93dab77251429a2a0d74
Celine ten Dam <celine.ten.dam@nav.no>
2022-05-20 08:23:37 +0200

    Regeltre for tilleggsopplysninger
    DAG-256

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Tilleggsopplysninger.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Tilleggsopplysninger.kt
@@ -1,17 +1,34 @@
 package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
 
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.boolsk
 import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.tekst
 import no.nav.dagpenger.model.faktum.Rolle
 import no.nav.dagpenger.model.faktum.Søknad
 import no.nav.dagpenger.model.faktum.Søknad.Companion.seksjon
+import no.nav.dagpenger.model.regel.er
+import no.nav.dagpenger.model.regel.utfylt
+import no.nav.dagpenger.model.subsumsjon.hvisOppfylt
+import no.nav.dagpenger.model.subsumsjon.minstEnAv
 import no.nav.dagpenger.quiz.mediator.soknad.DslFaktaseksjon
 
 object Tilleggsopplysninger : DslFaktaseksjon {
     const val `tilleggsopplysninger` = 4001
+    const val `har tilleggsopplysninger` = 4002
 
     override val fakta = listOf(
-        tekst faktum "faktum.tilleggsopplysninger" id `tilleggsopplysninger`
+        tekst faktum "faktum.tilleggsopplysninger" id `tilleggsopplysninger` avhengerAv `har tilleggsopplysninger`,
+        boolsk faktum "faktum.tilleggsopplysninger.har-tilleggsopplysninger" id `har tilleggsopplysninger`
     )
 
-    override fun seksjon(søknad: Søknad) = listOf(søknad.seksjon("tilleggsopplysninger", Rolle.søker, *this.databaseIder()))
+    override fun seksjon(søknad: Søknad) =
+        listOf(søknad.seksjon("tilleggsopplysninger", Rolle.søker, *this.databaseIder()))
+
+    fun regeltre(søknad: Søknad) = with(søknad) {
+        "har tilleggsopplysninger eller ikke".minstEnAv(
+            boolsk(`har tilleggsopplysninger`) er false,
+            boolsk(`har tilleggsopplysninger`) er true hvisOppfylt {
+                tekst(tilleggsopplysninger).utfylt()
+            }
+        )
+    }
 }
--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/TilleggsopplysningerTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/TilleggsopplysningerTest.kt
@@ -1,12 +1,57 @@
 package no.nav.dagpenger.quiz.mediator.soknad.dagpenger
 
+import no.nav.dagpenger.model.faktum.Faktum
+import no.nav.dagpenger.model.faktum.Prosessversjon
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.faktum.Tekst
+import no.nav.dagpenger.model.seksjon.Søknadprosess
+import no.nav.dagpenger.quiz.mediator.helpers.testSøknadprosess
+import no.nav.dagpenger.quiz.mediator.soknad.Prosess
 import no.nav.dagpenger.quiz.mediator.soknad.verifiserFeltsammensetting
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 internal class TilleggsopplysningerTest {
 
+    private val søknad = Søknad(Prosessversjon(Prosess.Dagpenger, -1), *Tilleggsopplysninger.fakta())
+    private lateinit var søknadprosess: Søknadprosess
+    private lateinit var harTilleggsopplysninger: Faktum<Boolean>
+    private lateinit var tilleggsopplysninger: Faktum<Tekst>
+
     @Test
     fun `Sjekk om faktasammensettingen har endret seg siden sist`() {
-        Tilleggsopplysninger.verifiserFeltsammensetting(1, 4001)
+        Tilleggsopplysninger.verifiserFeltsammensetting(2, 8003)
+    }
+
+    @BeforeEach
+    fun setup() {
+        søknadprosess = søknad.testSøknadprosess(Tilleggsopplysninger.regeltre(søknad))
+        harTilleggsopplysninger = søknadprosess.boolsk(Tilleggsopplysninger.`har tilleggsopplysninger`)
+        tilleggsopplysninger = søknadprosess.tekst(Tilleggsopplysninger.tilleggsopplysninger)
+    }
+
+    @Test
+    fun `Har tilleggsopplysninger eller ikke`() {
+        harTilleggsopplysninger.besvar(false)
+        assertEquals(true, søknadprosess.resultat())
+
+        harTilleggsopplysninger.besvar(true)
+        assertEquals(null, søknadprosess.resultat())
+
+        tilleggsopplysninger.besvar(Tekst("Tilleggsopplysninger"))
+        assertEquals(true, søknadprosess.resultat())
+    }
+
+    @Test
+    fun avhengigheter() {
+        harTilleggsopplysninger.besvar(true)
+        tilleggsopplysninger.besvar(Tekst("Tilleggsopplysninger"))
+        assertTrue(tilleggsopplysninger.erBesvart())
+
+        harTilleggsopplysninger.besvar(false)
+        assertFalse(tilleggsopplysninger.erBesvart())
     }
 }
