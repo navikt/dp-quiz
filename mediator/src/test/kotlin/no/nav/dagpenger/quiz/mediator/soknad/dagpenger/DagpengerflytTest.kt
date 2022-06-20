aff85f2aa2ac1d1ec1e5aafabca579baa34ef424
Celine ten Dam <celine.ten.dam@nav.no>
2022-06-20 10:39:15 +0200

    Fjernet kanEndresAv.
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Geir Andre Lund <geir.andre.lund@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/DagpengerflytTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/DagpengerflytTest.kt
@@ -33,18 +33,10 @@ class DagpengerflytTest {
         søknadprosess.dato("${BarnetilleggRegister.`barn foedselsdato register`}.1").besvar(LocalDate.now().minusYears(10))
         søknadprosess.land("${BarnetilleggRegister.`barn statsborgerskap register`}.1").besvar(Land("NOR"))
 
-        søknadprosess.nesteSeksjoner().onEach {
-            println(it.somSpørsmål())
-        }
-
         // Besvares av bruker
-        // søknadprosess.boolsk("${BarnetilleggRegister.`forsoerger du barnet register`}.1").besvar(false)
-        // søknadprosess.boolsk("${BarnetilleggRegister.`barn aarsinntekt over 1g register`}.1").besvar(false)
-        // søknadprosess.heltall("${BarnetilleggRegister.`barn inntekt register`}.1").besvar(0)
-
-        søknadprosess.nesteSeksjoner().onEach {
-            println(it.somSpørsmål())
-        }
+        søknadprosess.boolsk("${BarnetilleggRegister.`forsoerger du barnet register`}.1").besvar(false)
+        søknadprosess.boolsk("${BarnetilleggRegister.`barn aarsinntekt over 1g register`}.1").besvar(false)
+        søknadprosess.heltall("${BarnetilleggRegister.`barn inntekt register`}.1").besvar(0)
 
         søknadprosess.generator(BarnetilleggSøker.`barn liste`).besvar(0)
 
@@ -71,9 +63,6 @@ class DagpengerflytTest {
         søknadprosess.boolsk(ReellArbeidssoker.`Kan bytte yrke og eller gå ned i lønn`).besvar(true)
 
         søknadprosess.boolsk(Tilleggsopplysninger.`har tilleggsopplysninger`).besvar(false)
-        søknadprosess.nesteSeksjoner().onEach {
-            println(it.somSpørsmål())
-        }
         assertTrue(søknadprosess.erFerdig(), "Forventet at Dagpenger søknadsprosessen ikke var ferdig. Mangler svar på ${søknadprosess.nesteSeksjoner().flatten().joinToString { "\n$it" }}")
     }
 }
