8d3542bae08653d908877c171ed4db6a5f7db4c7
André Roaldseth <andre.roaldseth@nav.no>
2022-06-30 13:15:48 +0200

    Lag ny prosessversjon for dokumentasjonskrav

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/innsending/Innsending.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/innsending/Innsending.kt
@@ -12,7 +12,7 @@ import no.nav.dagpenger.quiz.mediator.soknad.innsending.Innsending.Subsumsjoner.
 
 internal object Innsending {
     private val logger = KotlinLogging.logger { }
-    val VERSJON_ID = Prosessversjon(Prosess.Innsending, 1)
+    val VERSJON_ID = Prosessversjon(Prosess.Innsending, 2)
 
     fun registrer(registrer: (prototype: Søknad) -> Unit) {
         registrer(prototypeSøknad)
