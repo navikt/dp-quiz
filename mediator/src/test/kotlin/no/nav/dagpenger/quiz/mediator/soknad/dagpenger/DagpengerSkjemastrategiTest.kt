f8b537f4053946e5acbd26ba78fd59052f6654e0
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2022-09-27 14:00:11 +0200

    Finner skjemakode for gjenopptak

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/DagpengerSkjemastrategiTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/DagpengerSkjemastrategiTest.kt
@@ -70,4 +70,55 @@ internal class DagpengerSkjemastrategiTest {
             assertEquals("Søknad om dagpenger (ikke permittert)", this.tittel)
         }
     }
+
+    @Test
+    fun `skjemakode gjenopptak permittering når gjenopptak og permittert`() {
+        søknadprosess.land(Bosted.`hvilket land bor du i`).besvar(Land("NOR"))
+        søknadprosess.envalg(Gjenopptak.`mottatt dagpenger siste 12 mnd`)
+            .besvar(Envalg("faktum.mottatt-dagpenger-siste-12-mnd.svar.ja"))
+        søknadprosess.generator(Barnetillegg.`barn liste register`).besvar(0)
+        søknadprosess.boolsk(Barnetillegg.`egne barn`).besvar(false)
+
+        søknadprosess.dato(Arbeidsforhold.`dagpenger søknadsdato`).besvar(1.januar)
+        søknadprosess.envalg(Arbeidsforhold.`type arbeidstid`).besvar(Envalg("faktum.type-arbeidstid.svar.fast"))
+
+        søknadprosess.generator(Arbeidsforhold.arbeidsforhold).besvar(2)
+        søknadprosess.tekst("${Arbeidsforhold.`arbeidsforhold navn bedrift`}.1").besvar(Tekst("navn"))
+        søknadprosess.land("${Arbeidsforhold.`arbeidsforhold land`}.1").besvar(Land("NOR"))
+        søknadprosess.envalg("${Arbeidsforhold.`arbeidsforhold endret`}.1")
+            .besvar(Envalg("faktum.arbeidsforhold.endret.svar.sagt-opp-av-arbeidsgiver"))
+
+        søknadprosess.tekst("${Arbeidsforhold.`arbeidsforhold navn bedrift`}.2").besvar(Tekst("navn"))
+        søknadprosess.land("${Arbeidsforhold.`arbeidsforhold land`}.2").besvar(Land("NOR"))
+        søknadprosess.envalg("${Arbeidsforhold.`arbeidsforhold endret`}.2")
+            .besvar(Envalg("faktum.arbeidsforhold.endret.svar.permittert"))
+
+        with(DagpengerSkjemastrategi().skjemakode(søknadprosess)) {
+            assertEquals("04-16.04", this.skjemakode)
+            assertEquals("Søknad om gjenopptak av dagpenger ved permittering", this.tittel)
+        }
+    }
+
+    @Test
+    fun `skjemakode gjenopptak når gjenopptak`() {
+        søknadprosess.land(Bosted.`hvilket land bor du i`).besvar(Land("NOR"))
+        søknadprosess.envalg(Gjenopptak.`mottatt dagpenger siste 12 mnd`)
+            .besvar(Envalg("faktum.mottatt-dagpenger-siste-12-mnd.svar.ja"))
+        søknadprosess.generator(Barnetillegg.`barn liste register`).besvar(0)
+        søknadprosess.boolsk(Barnetillegg.`egne barn`).besvar(false)
+
+        søknadprosess.dato(Arbeidsforhold.`dagpenger søknadsdato`).besvar(1.januar)
+        søknadprosess.envalg(Arbeidsforhold.`type arbeidstid`).besvar(Envalg("faktum.type-arbeidstid.svar.fast"))
+
+        søknadprosess.generator(Arbeidsforhold.arbeidsforhold).besvar(1)
+        søknadprosess.tekst("${Arbeidsforhold.`arbeidsforhold navn bedrift`}.1").besvar(Tekst("navn"))
+        søknadprosess.land("${Arbeidsforhold.`arbeidsforhold land`}.1").besvar(Land("NOR"))
+        søknadprosess.envalg("${Arbeidsforhold.`arbeidsforhold endret`}.1")
+            .besvar(Envalg("faktum.arbeidsforhold.endret.svar.sagt-opp-av-arbeidsgiver"))
+
+        with(DagpengerSkjemastrategi().skjemakode(søknadprosess)) {
+            assertEquals("04-16.03", this.skjemakode)
+            assertEquals("Søknad om gjenopptak av dagpenger", this.tittel)
+        }
+    }
 }
