1c1713a565e132fc3417d98fa60e607151f2aefd
André Roaldseth <andre.roaldseth@nav.no>
2022-11-03 10:13:24 +0100

    Lag en komplett prosess for versjon 2

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/db/SøknadRecordTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/db/SøknadRecordTest.kt
@@ -25,6 +25,7 @@ import no.nav.dagpenger.model.seksjon.Versjon.UserInterfaceType.Web
 import no.nav.dagpenger.quiz.mediator.db.PostgresDataSourceBuilder.dataSource
 import no.nav.dagpenger.quiz.mediator.helpers.Postgres
 import no.nav.dagpenger.quiz.mediator.helpers.SøknadEksempel1
+import no.nav.dagpenger.quiz.mediator.helpers.SøknadEksempel2
 import no.nav.dagpenger.quiz.mediator.helpers.Testprosess
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
@@ -376,11 +377,11 @@ internal class SøknadRecordTest {
 
             originalSøknadprosess.desimaltall("f26").besvar(9.9)
 
-            SøknadEksempel1.v2
-            FaktumTable(SøknadEksempel1.prototypeFakta2)
-            val nyProsessVersjon = søknadRecord.migrer(soknadUUID, SøknadEksempel1.prosessVersjon2)
+            SøknadEksempel2.v2
+            FaktumTable(SøknadEksempel2.prototypeFakta)
+            val nyProsessVersjon = søknadRecord.migrer(soknadUUID, SøknadEksempel2.prosessVersjon)
 
-            assertEquals(SøknadEksempel1.prosessVersjon2, nyProsessVersjon)
+            assertEquals(SøknadEksempel2.prosessVersjon, nyProsessVersjon)
 
             with(søknadRecord.hent(soknadUUID)) {
                 assertFalse(heltall("f26").erBesvart())
@@ -388,6 +389,13 @@ internal class SøknadRecordTest {
 
                 heltall("f26").besvar(12)
                 desimaltall("f27").besvar(1.3)
+                envalg("f28").besvar(Envalg("f28.valg1"))
+                tekst("f20").besvar(Tekst("Foo"))
+
+                assertThrows<IllegalArgumentException> {
+                    // Slettet og finnes ikke lenger
+                    tekst("f23").besvar(Tekst("Foo"))
+                }
 
                 assertTrue(desimaltall("f27").erBesvart())
                 søknadRecord.lagre(this.søknad)
---
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/helpers/SøknadEksempel2.kt
@@ -0,0 +1,89 @@
+package no.nav.dagpenger.quiz.mediator.helpers
+
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.boolsk
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.dato
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.desimaltall
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.dokument
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.envalg
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.flervalg
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.heltall
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.inntekt
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.land
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.periode
+import no.nav.dagpenger.model.factory.BaseFaktumFactory.Companion.tekst
+import no.nav.dagpenger.model.factory.UtledetFaktumFactory.Companion.maks
+import no.nav.dagpenger.model.faktum.Prosessversjon
+import no.nav.dagpenger.model.faktum.Rolle
+import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.regel.er
+import no.nav.dagpenger.model.seksjon.Seksjon
+import no.nav.dagpenger.model.seksjon.Søknadprosess
+import no.nav.dagpenger.model.seksjon.Versjon
+
+internal object SøknadEksempel2 {
+    val prosessVersjon = Prosessversjon(Testprosess.Test, 889)
+    internal val prototypeFakta by lazy {
+        Søknad(
+            prosessVersjon,
+            boolsk faktum "f1" id 1 avhengerAv 11,
+            dato faktum "f2" id 2,
+            dato faktum "f3" id 3,
+            dato faktum "f4" id 4,
+            dato faktum "f5" id 5,
+            inntekt faktum "f6" id 6,
+            inntekt faktum "f7" id 7,
+            inntekt faktum "f8" id 8,
+            inntekt faktum "f9" id 9,
+            boolsk faktum "f10" id 10,
+            dokument faktum "f11" id 11,
+            boolsk faktum "f12" id 12 avhengerAv 11,
+            dato faktum "f13" id 13,
+            boolsk faktum "f14" id 14,
+            heltall faktum "f15" id 15 genererer 16 og 17 og 18 avhengerAv 14,
+            heltall faktum "f16" id 16,
+            boolsk faktum "f17" id 17,
+            boolsk faktum "f18" id 18,
+            boolsk faktum "f19" id 19 avhengerAv 2 og 13,
+            maks dato "345" av 3 og 4 og 5 id 345,
+            maks dato "345213" av 345 og 2 og 13 id 345213,
+            tekst faktum "f20" med "envalg1" med "envalg2" id 20,
+            flervalg faktum "f21" med "flervalg1" med "flervalg2" med "flervalg3" id 21,
+            heltall faktum "f22" id 22,
+            periode faktum "f24" id 24,
+            land faktum "f25" id 25,
+            heltall faktum "f26" id 26,
+            desimaltall faktum "f27" id 27,
+            envalg faktum "f28" med "valg1" med "valg2" id 28
+        )
+    }
+    private val webPrototypeSøknad = Søknadprosess(
+        Seksjon(
+            "seksjon",
+            Rolle.søker,
+            *(prototypeFakta.map { it }.toTypedArray())
+        )
+    )
+    private val mobilePrototypeSøknad = Søknadprosess(
+        Seksjon(
+            "seksjon",
+            Rolle.søker,
+            *(prototypeFakta.map { it }.toTypedArray())
+        ),
+        Seksjon(
+            "template seksjon",
+            Rolle.søker,
+            prototypeFakta.heltall(16),
+            prototypeFakta.boolsk(17)
+        )
+    )
+    val v2 by lazy {
+        Versjon.Bygger(
+            prototypeFakta,
+            prototypeFakta boolsk 1 er true,
+            mapOf(
+                Versjon.UserInterfaceType.Web to webPrototypeSøknad,
+                Versjon.UserInterfaceType.Mobile to mobilePrototypeSøknad
+            )
+        ).registrer()
+    }
+}
