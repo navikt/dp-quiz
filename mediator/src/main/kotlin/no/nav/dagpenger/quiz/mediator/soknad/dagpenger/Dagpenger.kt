b2b0504eaed2e4d93d04aaaa6db4f1dbb9a7010b
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2022-11-10 15:36:36 +0100

    ENDA en ny versjon for å teste migrering
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Dagpenger.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/quiz/mediator/soknad/dagpenger/Dagpenger.kt
@@ -20,7 +20,7 @@ internal object Dagpenger {
      *
      * Dette for at innsendte søknader fortsatt skal kunne lastes, uten å bli migrert fram.
      */
-    val VERSJON_ID = Prosessversjon(Prosess.Dagpenger, 241)
+    val VERSJON_ID = Prosessversjon(Prosess.Dagpenger, 242)
 
     fun registrer(registrer: (prototype: Søknad) -> Unit) {
         registrer(prototypeSøknad)
