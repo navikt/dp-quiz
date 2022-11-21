0e815a9db4af29caf7fdb26d2691be805fd8d747
André Roaldseth <andre.roaldseth@nav.no>
2022-11-21 15:44:25 +0100

    Filtere ut templates fordi de deepclones
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

--- model/src/main/kotlin/no/nav/dagpenger/model/subsumsjon/SannsynliggjøringsSubsumsjon.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/subsumsjon/SannsynliggjøringsSubsumsjon.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.model.subsumsjon
 import no.nav.dagpenger.model.faktum.Faktum
 import no.nav.dagpenger.model.faktum.GrunnleggendeFaktum
 import no.nav.dagpenger.model.faktum.Søknad
+import no.nav.dagpenger.model.faktum.TemplateFaktum
 import no.nav.dagpenger.model.seksjon.Søknadprosess
 import no.nav.dagpenger.model.visitor.SubsumsjonVisitor
 
@@ -51,11 +52,12 @@ class SannsynliggjøringsSubsumsjon private constructor(
     }
 
     override fun bygg(søknad: Søknad): SannsynliggjøringsSubsumsjon {
+        val kopiertSubsumsjon = child.bygg(søknad)
         val kopierteSannsynliggjøringsFakta =
             sannsynliggjøringsFakta.map { faktum -> søknad.dokument(faktum.id) }.toSet()
-        val kopiertSubsumsjon = child.bygg(søknad)
 
-        kopiertSubsumsjon.alleFakta().forEach { faktum ->
+        kopiertSubsumsjon.alleFakta().filterNot { it is TemplateFaktum<*> }.forEach { faktum ->
+            // TODO: Templates blir klonet og får med seg avhengigheter fra første runde, mens grunnleggende må legges til eksplsitt
             faktum.sannsynliggjøresAv(kopierteSannsynliggjøringsFakta)
         }
         return SannsynliggjøringsSubsumsjon(
