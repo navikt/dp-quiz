47670063f0348472d97f38ee66b850527cab09ec
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2022-12-14 15:21:29 +0100

    PDL sender landkoden XXX i tilfeller hvor landet er ukjent.
    Dermed må dette også tillates i Quiz som et gyldig landvalg.

--- model/src/main/kotlin/no/nav/dagpenger/model/faktum/Land.kt
+++ model/src/main/kotlin/no/nav/dagpenger/model/faktum/Land.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.model.faktum
 class Land(alpha3Code: String) : Comparable<Land> {
     companion object {
         internal val gyldigeLand = LandOppslag.land()
+        internal val pdlKodeForUkjentLand = "XXX"
     }
 
     val alpha3Code: String
@@ -11,12 +12,17 @@ class Land(alpha3Code: String) : Comparable<Land> {
         require(alpha3Code.length == 3) {
             "ISO 3166-1-alpha3 må være 3 bokstaver lang. Fikk: $alpha3Code"
         }
-        require(LandOppslag.fraAlpha3Code(alpha3Code) != null) {
-            "Ugyldig land kode: $alpha3Code"
+
+        if (alpha3Code.erAntattKjentLand()) {
+            require(LandOppslag.fraAlpha3Code(alpha3Code) != null) {
+                "Ugyldig land kode: $alpha3Code"
+            }
         }
 
         this.alpha3Code = alpha3Code.uppercase()
     }
+
+    private fun String.erAntattKjentLand() = !pdlKodeForUkjentLand.equals(this, ignoreCase = true)
 
     override fun compareTo(other: Land): Int {
         return this.alpha3Code.compareTo(other.alpha3Code)
