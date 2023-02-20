b66c6e209f65cfa7f8b996487669991c265ae436
André Roaldseth <andre.roaldseth@nav.no>
2023-02-20 15:41:45 +0100

    Lag en testcase som framprovoserer de manglende template-faktumene
    Co-authored-by: Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/integration/UtredningsprosessTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/integration/UtredningsprosessTest.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.quiz.mediator.integration
 import no.nav.dagpenger.model.faktum.Envalg
 import no.nav.dagpenger.model.faktum.Identer
 import no.nav.dagpenger.model.faktum.Land
+import no.nav.dagpenger.model.faktum.Tekst
 import no.nav.dagpenger.model.seksjon.Utredningsprosess
 import no.nav.dagpenger.quiz.mediator.db.FaktumTable
 import no.nav.dagpenger.quiz.mediator.db.UtredningsprosessRepositoryImpl
@@ -14,6 +15,7 @@ import no.nav.dagpenger.quiz.mediator.soknad.dagpenger.DinSituasjon
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
+import java.util.UUID
 import kotlin.test.assertEquals
 
 internal class UtredningsprosessTest() {
@@ -25,27 +27,40 @@ internal class UtredningsprosessTest() {
             Dagpenger.registrer { fakta ->
                 FaktumTable(fakta)
             }
-            søknadsprosess = UtredningsprosessRepositoryImpl().ny(
+            val repository = UtredningsprosessRepositoryImpl()
+            val uuid = UUID.randomUUID()
+            søknadsprosess = repository.ny(
                 Identer(identer = setOf(Identer.Ident(Identer.Ident.Type.FOLKEREGISTERIDENT, "12312312311", false))),
                 Dagpenger.VERSJON_ID,
+                uuid,
             )
             medSeksjon(Bosted) {
                 it.land(`hvilket land bor du i`).besvar(Land("NOR"))
             }
             medSeksjon(DinSituasjon) {
+                it.harNavn("din-situasjon")
                 it.envalg(`mottatt dagpenger siste 12 mnd`)
                     .besvar(Envalg("faktum.mottatt-dagpenger-siste-12-mnd.svar.nei"))
                 it.dato(`dagpenger søknadsdato`).besvar(LocalDate.now())
                 it.envalg(`type arbeidstid`).besvar(Envalg("faktum.type-arbeidstid.svar.fast"))
-                it.heltall(arbeidsforhold).besvar(1)
+                it.generator(arbeidsforhold).besvar(1)
+                it.tekst("$`arbeidsforhold navn bedrift`.1").besvar(Tekst("Hei"))
             }
+            repository.lagre(søknadsprosess)
+
+            søknadsprosess = repository.hent(uuid)
+            medSeksjon(DinSituasjon) {
+                it.land("$`arbeidsforhold land`.1").besvar(Land("NOR"))
+            }
+
             assertEquals(false, søknadsprosess.erFerdig())
         }
     }
 
+    private fun Utredningsprosess.harNavn(navn: String) = assertEquals(navn, nesteSeksjoner()[0].navn)
+
     private fun <T : DslFaktaseksjon> medSeksjon(faktaseksjon: T, block: T.(fakta: Utredningsprosess) -> Unit) {
         assertNotEquals(søknadsprosess.nesteSeksjoner().size, 0, "Har ikke neste seksjon")
-        assertEquals(faktaseksjon.seksjon(søknadsprosess.fakta)[0].navn, søknadsprosess.nesteSeksjoner()[0].navn)
         block(faktaseksjon, søknadsprosess)
     }
 }
