96ea5da1dd5cf98dd327e18e233d1b6adf44a2f0
André Roaldseth <andre.roaldseth@nav.no>
2023-03-01 12:34:55 +0100

    Legg til indekser for soknad_id
    Å bruke foreign keys lager ikke *index* i tabellen man refererer fra. Det må eksplisitt lages indekser, og de trenger vi når vi sletter

---
+++ mediator/src/main/resources/db/migration/V28__LEGG_TIL_INDEKS_SOKNAD_ID.sql
@@ -0,0 +1,7 @@
+CREATE INDEX ON dokument(soknad_id);
+CREATE INDEX ON resultat(soknad_id);
+CREATE INDEX ON manuell_behandling(soknad_id);
+CREATE INDEX ON valgte_verdier(soknad_id);
+CREATE INDEX ON periode(soknad_id);
+CREATE INDEX ON faktum_verdi(soknad_id);
+CREATE INDEX ON gammel_faktum_verdi(soknad_id);
