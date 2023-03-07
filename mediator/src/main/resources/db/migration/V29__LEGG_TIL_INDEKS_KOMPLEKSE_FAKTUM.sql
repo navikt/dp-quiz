3651928a3571ced2de1d93323296675530bc82e7
André Roaldseth <andre.roaldseth@nav.no>
2023-03-07 14:06:12 +0100

    Legg til indekser fra faktum_verdi mot støttetabellene

---
+++ mediator/src/main/resources/db/migration/V29__LEGG_TIL_INDEKS_KOMPLEKSE_FAKTUM.sql
@@ -0,0 +1,9 @@
+CREATE INDEX ON faktum_verdi(envalg_id);
+CREATE INDEX ON faktum_verdi(flervalg_id);
+CREATE INDEX ON faktum_verdi(periode_id);
+CREATE INDEX ON faktum_verdi(dokument_id);
+
+CREATE INDEX ON gammel_faktum_verdi(envalg_id);
+CREATE INDEX ON gammel_faktum_verdi(flervalg_id);
+CREATE INDEX ON gammel_faktum_verdi(periode_id);
+CREATE INDEX ON gammel_faktum_verdi(dokument_id);
