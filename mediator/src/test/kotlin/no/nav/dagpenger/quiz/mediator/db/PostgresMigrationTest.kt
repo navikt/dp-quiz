3fe156ba8128ec8b28f838fcccc8d3733cc3156b
André Roaldseth <andre.roaldseth@nav.no>
2023-03-08 09:21:09 +0100

    Rett opp i navn på migreringer

--- mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/quiz/mediator/db/PostgresMigrationTest.kt
@@ -11,7 +11,7 @@ internal class PostgresMigrationTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = runMigration()
-            assertEquals(33, migrations)
+            assertEquals(34, migrations)
         }
     }
 }
