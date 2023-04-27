4b1f63f0fb06326538ad12e57a775666990595ba
Giao The Cung <giaothe@gmail.com>
2023-04-27 10:25:14 +0200

    Fikse spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ allprojects {
         // using idea found at
         // <https://github.com/diffplug/spotless/issues/1527#issuecomment-1409142798>.
         lineEndings = LineEnding.PLATFORM_NATIVE // or any other except GIT_ATTRIBUTES
-      }
+    }
 
     tasks.withType<Test> {
         useJUnitPlatform()
