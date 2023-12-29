8049b6a0b4a053da876e99a3b70625689f603204
André Roaldseth <andre.roaldseth@nav.no>
2023-12-29 14:39:53 +0100

    Bytt til dp-version-catalog (#180)

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -4,16 +4,16 @@ plugins {
 }
 
 dependencies {
-    implementation(Jackson.kotlin)
+    implementation(libs.jackson.kotlin)
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
 
     implementation("guru.nidi:graphviz-java:0.18.1")
     implementation("guru.nidi:graphviz-kotlin:0.18.1")
     api("de.slub-dresden:urnlib:2.0.1")
-    testImplementation(Mockk.mockk)
 
-    testFixturesApi(Jackson.kotlin)
-    testFixturesApi(Jackson.jsr310)
+    testImplementation(libs.mockk)
+    testFixturesApi(libs.jackson.kotlin)
+    testFixturesApi(libs.jackson.datatype.jsr310)
     testFixturesApi(Junit5.api)
     testFixturesApi("org.jetbrains.kotlin:kotlin-test:1.9.22")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,14 @@ pluginManagement {
 }
 
 include("mediator", "model")
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.dagpenger:dp-version-catalog:20231229.65.2c2e9f")
+        }
+    }
+}
