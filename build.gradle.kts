5ad427efa4280dfe2d43d05672ab3d9d2b549aa0
André Roaldseth <andre.roaldseth@nav.no>
2024-07-24 14:01:12 +0200

    Erstatt jitpack med github repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,9 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     repositories {
         mavenCentral()
-        maven("https://jitpack.io")
+        maven {
+            url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        }
     }
 
     tasks.named("compileKotlin") {
